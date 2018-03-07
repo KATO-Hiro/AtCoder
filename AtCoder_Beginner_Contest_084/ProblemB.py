7812fe54cefe6b07fb87e2abe0db305e53c46a6d
Kato Hiroki <k.hiro1818@gmail.com>
2018-03-07 22:34:07 +0900

    FIXYOU: All tests have passed.

--- AtCoder_Beginner_Contest_084/ProblemB.py
+++ AtCoder_Beginner_Contest_084/ProblemB.py
@@ -17,13 +17,13 @@
 if __name__ == '__main__':
     import re
 
-    A, B = list(map(int, input().split()))
+    A, B = list(map(str, input().split()))
     S = str(input())
 
-    # XXX: pattern is something wrong.
-    text = r'(\d{{0}})\-(\d{{1}})'.format(A, B)
-    pattern = re.compile(text)
-    result = pattern.search(S)
+    # See:
+    # https://beta.atcoder.jp/contests/abc084/submissions/1936327
+    text = r'\d{' + A + '}\-\d{' + B + '}'
+    result = re.match(text, S)
 
     if result:
         print("Yes")
