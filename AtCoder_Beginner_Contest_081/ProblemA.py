5e1193b10cfc22206271c097c260baf073b9e398
Kato Hiroki <k.hiro1818@gmail.com>
2018-03-09 23:12:41 +0900

    Initial commit.

---
+++ AtCoder_Beginner_Contest_081/ProblemA.py
@@ -0,0 +1,12 @@
+'''input
+101
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem A
+
+if __name__ == '__main__':
+    marble_count = sum(map(int, list(str(input()))))
+    print(marble_count)
