4c1c73f78560dd82b9f3a3f6294d391cc6ef0955
Kato Hiroki <k.hiro1818@gmail.com>
2018-03-15 22:20:37 +0900

    Initial commit.

---
+++ AtCoder_Beginner_Contest_069/ProblemA.py
@@ -0,0 +1,15 @@
+'''input
+3 4
+
+6
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem A
+
+if __name__ == '__main__':
+    n, m = list(map(int, input().split()))
+
+    print((n - 1) * (m - 1))
---
+++ AtCoder_Beginner_Contest_069/ProblemB.py
@@ -0,0 +1,13 @@
+'''input
+internationalization
+i18n
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem B
+
+if __name__ == '__main__':
+    s = input()
+    print(s[0] + str(len(s) - 2) + s[-1])
