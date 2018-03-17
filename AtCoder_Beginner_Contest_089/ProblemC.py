336c5d21bc106f069e2368814ef50efb6484aadc
Kato Hiroki <k.hiro1818@gmail.com>
2018-03-17 20:17:11 +0900

    FIXYOU: All test have passed.

--- AtCoder_Beginner_Contest_089/ProblemC.py
+++ AtCoder_Beginner_Contest_089/ProblemC.py
@@ -1,4 +1,30 @@
 '''input
+4
+ZZ
+ZZZ
+Z
+ZZZZZZZZZZ
+
+0
+
+5
+CHOKUDAI
+RNG
+MAKOTO
+AOKI
+RINGO
+
+7
+
+5
+MASHIKE
+RUMOI
+OBIRA
+HABORO
+HOROKANAI
+
+2
+
 '''
 
 # -*- coding: utf-8 -*-
@@ -6,36 +32,36 @@
 # AtCoder Beginner Contest
 # Problem C
 
-if __name__ == '__main__':
-    people_count = int(input())
 
-    person_name = list()
-
-    for person in range(people_count):
-        person_name.append(str(input()))
-
-    initial_strings = list()
-
-    for name in person_name:
-        initial_strings.append(name[0])
-
-    mod_initial_strings = set(initial_strings)
-
-    count = 0
-
-    matching_strings = ['M', 'A', 'R', 'C', 'H']
+def count_initial_name(people_count: int) -> dict:
     matching_strings_count = {'M': 0, 'A': 0, 'R': 0, 'C': 0, 'H': 0}
 
-    for string in matching_strings:
-        if string in mod_initial_strings:
-            count += initial_strings.count(string)
+    for person in range(people_count):
+        initial_name = input()[0]
 
-    for string in initial_strings:
-        matching_strings_count[string] += 1
+        if initial_name in matching_strings_count.keys():
+            matching_strings_count[initial_name] += 1
 
-    # TODO: Calc combination.
+    return matching_strings_count
 
-    if count >= 3:
-        print(matching_strings_count)
+
+def count_patterns(matching_strings_count):
+    import itertools
+
+    patterns = itertools.combinations(matching_strings_count.keys(), 3)
+    pattern_count = 0
+
+    for a_pattern in patterns:
+        pattern_count += matching_strings_count[a_pattern[0]] * \
+            matching_strings_count[a_pattern[1]] * \
+            matching_strings_count[a_pattern[2]]
+    return pattern_count
+
+
+if __name__ == '__main__':
+    matching_strings_count = count_initial_name(people_count=int(input()))
+
+    if sum(matching_strings_count.values()) >= 3:
+        print(count_patterns(matching_strings_count))
     else:
         print(0)
