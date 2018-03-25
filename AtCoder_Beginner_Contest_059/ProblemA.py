d96244122a24ad4e5efcadf1c9b50bd6e88176a6
Kato Hiroki <k.hiro1818@gmail.com>
2018-03-25 17:42:24 +0900

    Initial commit.

---
+++ AtCoder_Beginner_Contest_059/ProblemA.py
@@ -0,0 +1,15 @@
+'''input
+atcoder beginner contest
+ABC
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem A
+
+if __name__ == '__main__':
+    a, b, c = input().split()
+    result = a[0] + b[0] + c[0]
+    print(result.upper())
