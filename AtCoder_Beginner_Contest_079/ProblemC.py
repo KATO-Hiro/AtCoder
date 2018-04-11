60dcd63809429c912a213b7ac11d26f5a13e3af0
Kato Hiroki <k.hiro1818@gmail.com>
2018-04-11 09:53:37 +0900

    Refactoring ABC079 task C.

--- AtCoder_Beginner_Contest_079/ProblemC.py
+++ AtCoder_Beginner_Contest_079/ProblemC.py
@@ -16,28 +16,21 @@
 # Problem C
 
 if __name__ == '__main__':
-    numbers = list(map(int, list(input())))
+    numbers = list(input())
     result = '=7'
     a = numbers[0]
     b = numbers[1]
     c = numbers[2]
     d = numbers[3]
-    total = 7
 
-    # FIME: More smartly.
-    if a + b + c + d == total:
-        print(str(a) + '+' + str(b) + '+' + str(c) + '+' + str(d) + result)
-    elif a + b + c - d == total:
-        print(str(a) + '+' + str(b) + '+' + str(c) + '-' + str(d) + result)
-    elif a + b - c + d == total:
-        print(str(a) + '+' + str(b) + '-' + str(c) + '+' + str(d) + result)
-    elif a + b - c - d == total:
-        print(str(a) + '+' + str(b) + '-' + str(c) + '-' + str(d) + result)
-    elif a - b + c + d == total:
-        print(str(a) + '-' + str(b) + '+' + str(c) + '+' + str(d) + result)
-    elif a - b + c - d == total:
-        print(str(a) + '-' + str(b) + '+' + str(c) + '-' + str(d) + result)
-    elif a - b - c + d == total:
-        print(str(a) + '-' + str(b) + '-' + str(c) + '+' + str(d) + result)
-    elif a - b - c - d == total:
-        print(str(a) + '-' + str(b) + '-' + str(c) + '-' + str(d) + result)
+    # See:
+    # https://abc079.contest.atcoder.jp/submissions/1917934
+    ops = ['+', '-']
+
+    for op1 in ops:
+        for op2 in ops:
+            for op3 in ops:
+                expression = a + op1 + b + op2 + c + op3 + d
+                if eval(expression) == 7:
+                    print(expression + '=7')
+                    exit()
