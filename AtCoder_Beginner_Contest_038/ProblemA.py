9ee979c80e712b175085cd13059d190b226d77f3
Kato Hiroki <k.hiro1818@gmail.com>
2018-04-24 12:25:46 +0900

    Initial commit.

---
+++ AtCoder_Beginner_Contest_038/ProblemA.py
@@ -0,0 +1,21 @@
+'''input
+MUGICHA
+NO
+
+ICEDT
+YES
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem A
+
+if __name__ == '__main__':
+    s = input()
+
+    if s[-1] == 'T':
+        print('YES')
+    else:
+        print('NO')
