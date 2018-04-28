00383ed4a359d5021ede126094120d734a028de5
Kato Hiroki <k.hiro1818@gmail.com>
2018-04-28 23:30:42 +0900

    Initial commit.

---
+++ AtCoder_Beginner_Contest_039/ProblemB.py
@@ -0,0 +1,21 @@
+'''input
+390625
+390625
+
+1
+1
+
+981506241
+177
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem B
+
+if __name__ == '__main__':
+    from math import sqrt
+    n = int(input())
+    print(int(sqrt(sqrt(n))))
