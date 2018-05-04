77217cd0fec3e8877fbf0ba63eb14e8ae6257e36
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-04 18:43:58 +0900

    Refactoring ABC065 task C.

--- AtCoder_Beginner_Contest_065/ProblemC.py
+++ AtCoder_Beginner_Contest_065/ProblemC.py
@@ -17,25 +17,29 @@
 
 # AtCoder Beginner Contest
 # Problem C
+large_number = 10 ** 9 + 7
+
+
+def factorial(number: int) -> int:
+    value = 1
+
+    for i in range(1, number + 1):
+        value = (value * i) % large_number
+    return value
+
 
 if __name__ == '__main__':
     n, m = list(map(int, input().split()))
     diff = abs(n - m)
-    n_factorial = 1
-    m_factorial = 1
-    large_number = 10 ** 9 + 7
+    result = factorial(min(n, m))
 
-    for i in range(1, n + 1):
-        n_factorial = (n_factorial * i) % large_number
-
-    for k in range(1, m + 1):
-        m_factorial = (m_factorial * k) % large_number
-
-    rate = 0
-
+    # See:
+    # https://beta.atcoder.jp/contests/abc065/submissions/1654425
     if diff == 1:
         rate = 1
+        print(((result * result * max(n, m) * rate) % large_number))
     elif diff == 0:
         rate = 2
-
-    print(((n_factorial * m_factorial * rate) % large_number))
+        print(((result * result * rate) % large_number))
+    else:
+        print(0)
