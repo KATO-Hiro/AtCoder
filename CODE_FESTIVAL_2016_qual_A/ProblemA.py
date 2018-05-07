90e5b231a2d1d252ac37455c32eba5bfcf95a143
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-07 12:33:16 +0900

    Initial commit.

---
+++ CODE_FESTIVAL_2016_qual_A/ProblemA.py
@@ -0,0 +1,14 @@
+'''input
+CODEFESTIVAL
+CODE FESTIVAL
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# CODE FESTIVAL 2016 qual A
+# Problem A
+
+if __name__ == '__main__':
+    s = input()
+    print(s[:4], s[4:])
