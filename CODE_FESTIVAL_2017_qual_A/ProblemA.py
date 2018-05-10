8183de09e6284c382b11522798cb43d4edf45305
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-10 12:16:12 +0900

    Initial commit.

---
+++ CODE_FESTIVAL_2017_qual_A/ProblemA.py
@@ -0,0 +1,21 @@
+'''input
+TAKOYAKI
+No
+
+YAKINIKU
+Yes
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# CODE FESTIVAL 2017 qual A
+# Problem A
+
+if __name__ == '__main__':
+    s = input()
+
+    if s[:4] == 'YAKI':
+        print('Yes')
+    else:
+        print('No')
