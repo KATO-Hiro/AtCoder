1584aba224dd19109cf22a11ecbf6f574e444f1e
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-11 12:26:57 +0900

    Initial commit.

---
+++ CODE_FESTIVAL_2017_qual_B/ProblemA.py
@@ -0,0 +1,15 @@
+'''input
+CODEFESTIVAL
+CODE
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# CODE FESTIVAL 2017 qual B
+# Problem A
+
+if __name__ == '__main__':
+    s = input()
+
+    print(s[:len(s) - len('FESTIVAL')])
