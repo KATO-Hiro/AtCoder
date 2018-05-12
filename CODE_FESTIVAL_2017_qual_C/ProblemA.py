8e033e2c1da10989161973e180e07f4887e78f0e
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-13 07:43:58 +0900

    Initial commit.

---
+++ CODE_FESTIVAL_2017_qual_C/ProblemA.py
@@ -0,0 +1,21 @@
+'''input
+ABCD
+No
+
+BACD
+Yes
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# CODE FESTIVAL 2017 qual C
+# Problem A
+
+if __name__ == '__main__':
+    s = input()
+
+    if 'AC' in s:
+        print('Yes')
+    else:
+        print('No')
