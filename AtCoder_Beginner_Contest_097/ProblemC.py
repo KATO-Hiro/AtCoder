c2faaeb4c62d3a597de0d1d088566ce28e7be4dd
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-12 22:27:22 +0900

    Initial commit.

---
+++ AtCoder_Beginner_Contest_097/ProblemC.py
@@ -0,0 +1,41 @@
+'''input
+z
+1
+z
+
+aba
+4
+b
+
+atcoderandatcodeer
+5
+andat
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# AtCoder Beginner Contest
+# Problem C
+
+if __name__ == '__main__':
+    s = input()
+    k = int(input())
+    extracted = list()
+    s_len = len(s)
+
+    for i in range(1, s_len + 1):
+        start = 0
+        ended = i
+
+        for j in range(s_len - i + 1):
+            sub_str = s[start:ended]
+
+            if sub_str not in extracted:
+                extracted.append(sub_str)
+
+            start += 1
+            ended += 1
+
+    sorted_extracted = sorted(extracted)
+    print(sorted_extracted[k - 1])
