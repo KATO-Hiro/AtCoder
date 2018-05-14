0598acee241da2f346631f65f5601b14ae0dc78b
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-14 12:20:17 +0900

    Initial commit.

--- CODE_FESTIVAL_2017_qual_B/ProblemA.py
+++ CODE_FESTIVAL_2017_qual_B/ProblemA.py
@@ -1,15 +1,42 @@
 '''input
-CODEFESTIVAL
-CODE
+0
+0
+0
+0
+0
+0
+0
+0
+0
+
+240
+600
+1800
+3600
+4800
+7200
+10000
+0
+10000
+
+10400
+10300
+10100
+9800
+9500
+8500
+7000
+5000
+10400
+10400
 
 '''
 
 # -*- coding: utf-8 -*-
 
-# CODE FESTIVAL 2017 qual B
+# CODE THANKS FESTIVAL 2017 A
 # Problem A
 
 if __name__ == '__main__':
-    s = input()
-
-    print(s[:len(s) - len('FESTIVAL')])
+    ts = [int(input()) for _ in range(8)]
+    print(max(ts))
