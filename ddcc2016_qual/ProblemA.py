73dc3d76188d838bf9a56b8d56ab487d0faaee33
Kato Hiroki <k.hiro1818@gmail.com>
2018-05-28 13:45:10 +0900

    Initial commit.

---
+++ ddcc2016_qual/ProblemA.py
@@ -0,0 +1,17 @@
+'''input
+90 120 100
+133.3333333333333333
+
+2 3 5
+7.5000000000000000
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# ddcc2017 qual
+# Problem A
+
+if __name__ == '__main__':
+    a, b, c = list(map(int, input().split()))
+    print(b / a * c)
