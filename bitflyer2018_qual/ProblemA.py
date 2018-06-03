221429144cfc85e5038f9d0f154de0fbac15ebd7
Kato Hiroki <k.hiro1818@gmail.com>
2018-06-03 16:16:05 +0900

    Refactoring bitflyer2018 task A.

--- bitflyer2018_qual/ProblemA.py
+++ bitflyer2018_qual/ProblemA.py
@@ -21,8 +21,4 @@
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-
-    for i in range(a, -1, -1):
-        if i % b == 0:
-            print(i)
-            exit()
+    print(a // b * b)
