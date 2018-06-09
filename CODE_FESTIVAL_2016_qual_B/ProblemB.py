b7fb9c21f9aa26cc6413a786269c5adbf8fc6ef3
Kato Hiroki <k.hiro1818@gmail.com>
2018-06-09 16:03:51 +0900

    Initial commit.

---
+++ CODE_FESTIVAL_2016_qual_B/ProblemB.py
@@ -0,0 +1,62 @@
+'''input
+5 2 2
+ccccc
+No
+No
+No
+No
+No
+
+10 2 3
+abccabaabb
+Yes
+Yes
+No
+No
+Yes
+Yes
+Yes
+No
+No
+No
+
+12 5 2
+cabbabaacaba
+No
+Yes
+Yes
+Yes
+Yes
+No
+Yes
+Yes
+No
+Yes
+No
+No
+
+'''
+
+# -*- coding: utf-8 -*-
+
+# CODE FESTIVAL 2016 qual A
+# Problem B
+
+if __name__ == '__main__':
+    n, a, b = list(map(int, input().split()))
+    s = input()
+    a_count = 0
+    b_count = 0
+    a_b_count = 0
+
+    for si in s:
+        if (si == 'a') and (a_b_count < a + b):
+            a_count += 1
+            print('Yes')
+        elif (si == 'b') and (a_b_count < a + b) and (b_count < b):
+            b_count += 1
+            print('Yes')
+        else:
+            print('No')
+
+        a_b_count = a_count + b_count
