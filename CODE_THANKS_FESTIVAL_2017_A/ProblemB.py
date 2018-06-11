c4234d0b513efa3c0eddbfb9ccfcb9c05fcc00d5
Kato Hiroki <k.hiro1818@gmail.com>
2018-06-11 22:00:49 +0900

    Refactoring CTF2017 task B.

--- CODE_THANKS_FESTIVAL_2017_A/ProblemB.py
+++ CODE_THANKS_FESTIVAL_2017_A/ProblemB.py
@@ -4,19 +4,13 @@
 
 if __name__ == '__main__':
     s = input()
+    i = 0
+    count = 0
 
-    if s == s[::-1]:
-        print(0)
-    else:
-        i = 0
-        count = 0
-
-        while len(s) > 0:
-            mod_s = s[i:]
-
-            if mod_s == mod_s[::-1]:
-                print(count)
-                exit()
-            else:
-                i += 1
-                count += 1
+    while len(s) > 0:
+        if s[i:] == s[i:][::-1]:
+            print(count)
+            exit()
+        else:
+            i += 1
+            count += 1
