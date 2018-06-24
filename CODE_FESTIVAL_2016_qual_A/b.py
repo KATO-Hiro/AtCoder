a6c498f46c3df99ce812faacde6e09d4325d8ff6
Kato Hiroki <k.hiro1818@gmail.com>
2018-06-24 21:24:05 +0900

    Refactoring CF2016A task B.

--- CODE_FESTIVAL_2016_qual_A/b.py
+++ CODE_FESTIVAL_2016_qual_A/b.py
@@ -3,13 +3,13 @@
 
 if __name__ == '__main__':
     n = int(input())
-    a = list(map(int, input().split()))
+    a = list(map(lambda x: int(x) - 1, input().split()))
     count = 0
 
     # See:
     # http://code-festival-2016-quala.contest.atcoder.jp/data/other/code-festival-2016-quala/editorial.pdf
     for i in range(len(a)):
-        if i == a[a[i] - 1] - 1:
+        if i == a[a[i]]:
             count += 1
 
     print(count // 2)
