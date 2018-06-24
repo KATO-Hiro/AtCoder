f12ad52c02cb2cdde628073520fab17c40d3df31
Kato Hiroki <k.hiro1818@gmail.com>
2018-06-24 13:45:31 +0900

    Refactoring ABC101 task C.

--- abc101/c.py
+++ abc101/c.py
@@ -2,18 +2,12 @@
 # AtCoder Beginner Contest
 
 
-# See:
-# http://terukizm.hatenablog.com/entry/20120505/1336218368
-def ceil(src, range):
-    return ((int)(src / range) + 1) * range
-
-
 if __name__ == '__main__':
     n, k = list(map(int, input().split()))
     a = list(map(int, input().split()))
-    tmp = (n - 1) % (k - 1)
+    ans = (n - 1) // (k - 1)
 
-    if tmp == 0:
-        print((n - 1) // (k - 1))
+    if (n - 1) % (k - 1) == 0:
+        print(ans)
     else:
-        print(ceil((n - 1) / (k - 1), 1))
+        print(ans + 1)
