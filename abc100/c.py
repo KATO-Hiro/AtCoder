f6938b99b8858f6311ff44f384eb581ae3fafed1
Kato Hiroki <k.hiro1818@gmail.com>
2018-06-27 16:23:06 +0900

    Refactoring ABC100 task C.

--- abc100/c.py
+++ abc100/c.py
@@ -7,12 +7,11 @@ if __name__ == '__main__':
     a = list(map(int, input().split()))
     count = 0
 
+    # See:
+    # https://beta.atcoder.jp/contests/abc100/submissions/2675457
     for ai in a:
-        while True:
-            if ai % 2 == 0:
-                ai = ai // 2
-                count += 1
-            else:
-                break
+        while ai % 2 == 0:
+            ai //= 2
+            count += 1
 
     print(count)
