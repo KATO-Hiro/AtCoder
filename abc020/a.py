e3643deaa7416b0bbb3485e991d95ed36c30b8db
Kato Hiroki <k.hiro1818@gmail.com>
2018-07-04 19:44:03 +0900

    Initial commit.

---
+++ abc020/a.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# AtCoder Beginner Contest
+
+
+if __name__ == '__main__':
+    q = int(input())
+
+    if q == 1:
+        print('ABC')
+    else:
+        print('chokudai')
