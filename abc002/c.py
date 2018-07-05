4d3c3b157d40b165e0220a15729bdaeff30bd352
Kato Hiroki <k.hiro1818@gmail.com>
2018-07-05 18:44:43 +0900

    Initial commit.

---
+++ abc002/c.py
@@ -0,0 +1,24 @@
+'''input
+298 520 903 520 4 663
+43257.5
+
+1 0 3 0 2 5
+5.0
+
+-1 -2 3 4 5 6
+2.0
+
+'''
+
+# -*- coding: utf-8 -*-
+# AtCoder Beginner Contest
+
+
+if __name__ == '__main__':
+    xa, ya, xb, yb, xc, yc = list(map(int, input().split()))
+    xb -= xa
+    xc -= xa
+    yb -= ya
+    yc -= ya
+
+    print(abs(xb * yc - yb * xc) / 2)
