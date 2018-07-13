d39f15d1e818eb9db8fd7ce8e7fd7cbfc72ed4a1
Kato Hiroki <k.hiro1818@gmail.com>
2018-07-13 12:55:39 +0900

    Initial commit.

---
+++ abc015/a.py
@@ -0,0 +1,23 @@
+'''input
+ttttiiiimmmmeeee
+time
+ttttiiiimmmmeeee
+
+isuruu
+isleapyear
+isleapyear
+
+'''
+
+# -*- coding: utf-8 -*-
+# AtCoder Beginner Contest
+
+
+if __name__ == '__main__':
+    a = input()
+    b = input()
+
+    if len(a) > len(b):
+        print(a)
+    else:
+        print(b)
