f0c056aa745c7f37a48b6cbbece9001710023e77
Kato Hiroki <k.hiro1818@gmail.com>
2018-07-18 19:56:33 +0900

    Refactoring AGC026 task A.

--- agc026/a.py
+++ agc026/a.py
@@ -5,16 +5,15 @@ if __name__ == '__main__':
     n = int(input())
     a = list(map(int, input().split()))
     magic_count = 0
-    slime_count = 1
     previous_slime = a[0]
 
+    # See:
+    # https://beta.atcoder.jp/contests/agc026/submissions/2841276
     for i in range(1, n):
         if a[i] == previous_slime:
-            slime_count += 1
+            magic_count += 1
+            previous_slime = -1
         else:
-            magic_count += slime_count // 2
-            slime_count = 1
+            previous_slime = a[i]
 
-        previous_slime = a[i]
-
-    print(magic_count + slime_count // 2)
+    print(magic_count)
