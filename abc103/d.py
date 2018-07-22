5dfbff5b944496f559617cd54344edd503d4ecbb
Kato Hiroki <k.hiro1818@gmail.com>
2018-07-22 15:04:20 +0900

    Refactoring ABC103 task D.

--- abc103/d.py
+++ abc103/d.py
@@ -3,22 +3,17 @@
 
 if __name__ == '__main__':
     n, m = list(map(int, input().split()))
-    ab = list()
+    ab = sorted([tuple(map(int, input().split())) for _ in range(m)],
+                key=lambda x: x[1])
     count = 1
 
     # See:
     # https://www.youtube.com/watch?v=nVSWen0oM38
-    for i in range(m):
-        ab.append(tuple(map(int, input().split())))
-
-    sorted_ab = sorted(ab, key=lambda x: x[1])
-    b = sorted_ab[0][1]
+    b = ab[0][1]
 
     for i in range(1, m):
-        if sorted_ab[i][0] < b <= sorted_ab[i][1]:
-            pass
-        else:
-            b = sorted_ab[i][1]
+        if ab[i][0] >= b:
+            b = ab[i][1]
             count += 1
 
     print(count)
