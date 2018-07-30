e3813e6d43781d83b0c11b461d1d53084cc1e3cd
Kato Hiroki <k.hiro1818@gmail.com>
2018-07-30 12:53:24 +0900

    Initial commit.

---
+++ abc009/a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+
+    if n % 2 == 0:
+        print(n // 2)
+    else:
+        print(n // 2 + 1)
+
+
+if __name__ == '__main__':
+    main()
