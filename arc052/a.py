0f45daf64c01a26c7740619c07bc9e396425cd13
Kato Hiroki <k.hiro1818@gmail.com>
2018-08-05 09:25:54 +0900

    Refactoring ARC052 task A.

--- arc052/a.py
+++ arc052/a.py
@@ -3,11 +3,10 @@
 
 def main():
     s = input()
-    numbers = [str(i) for i in range(0, 10)]
     result = ''
 
     for si in s:
-        if si in numbers:
+        if si.isdigit():
             result += si
 
     print(result)
