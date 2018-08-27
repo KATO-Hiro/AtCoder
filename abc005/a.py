0124edf3702cf1203166697288e03c8aa6f18bce
Kato Hiroki <k.hiro1818@gmail.com>
2018-08-27 12:20:03 +0900

    Initial commit.

---
+++ abc005/a.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    x, y = list(map(int, input().split()))
+    print(y // x)
+
+
+if __name__ == '__main__':
+    main()
---
+++ abc005/b.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+    t = [int(input()) for _ in range(n)]
+    print(min(t))
+
+
+if __name__ == '__main__':
+    main()
