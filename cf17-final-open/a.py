a92651eac2dd088c9afd8572269e18fb85eb8b56
Kato Hiroki <k.hiro1818@gmail.com>
2018-09-06 14:24:48 +0900

    Refactoring CF2017 final task A.

--- cf17-final-open/a.py
+++ cf17-final-open/a.py
@@ -5,18 +5,13 @@ def main():
     from re import search
 
     s = input()
-    removed_a = s.replace('A', '')
+    pattern = r'^A?KIHA?BA?RA?$'
+    is_match = search(pattern, s)
 
-    if (len(s) < 5 or len(s) < 10) and removed_a != 'KIHBR':
-        print('NO')
+    if is_match:
+        print('YES')
     else:
-        pattern = r'^A?KIHA?BA?RA?$'
-        is_match = search(pattern, s)
-
-        if is_match:
-            print('YES')
-        else:
-            print('NO')
+        print('NO')
 
 
 if __name__ == '__main__':
