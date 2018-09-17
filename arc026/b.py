2de63d68b2df1de02ccd904fb2931ff66ba1fb7c
Kato Hiroki <k.hiro1818@gmail.com>
2018-09-17 18:51:11 +0900

    Refactoring ARC026 task B.

--- arc026/b.py
+++ arc026/b.py
@@ -6,20 +6,17 @@ def main():
 
     n = int(input())
     numbers = set()
-    div_numbers = set()
 
     for i in range(1, int(sqrt(n)) + 1):
         if n % i == 0 and i != n:
             numbers.add(i)
 
-    for number in numbers:
-        p, q = divmod(n, number)
+        p, q = divmod(n, i)
 
         if q == 0 and p != n:
-            div_numbers.add(p)
+            numbers.add(p)
 
-    matched_numbers = numbers | div_numbers
-    total = sum(matched_numbers)
+    total = sum(numbers)
 
     if total == n:
         print('Perfect')
