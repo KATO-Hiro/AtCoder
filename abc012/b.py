fe5adbf12f8b4d36d1dc80b01dd8d4550090e682
Kato Hiroki <k.hiro1818@gmail.com>
2018-09-24 22:49:01 +0900

    Refactoring ABC012 task B.

--- abc012/b.py
+++ abc012/b.py
@@ -3,22 +3,13 @@
 
 def main():
     n = int(input())
+
+    # See:
+    # https://beta.atcoder.jp/contests/abc012/submissions/1352862
     hour = n // 3600
-    minute = (n - 3600 * hour) // 60
-    second = n - (3600 * hour) - (60 * minute)
-
-    str_hour = str(hour)
-    str_minute = str(minute)
-    str_second = str(second)
-
-    if len(str_hour) == 1:
-        str_hour = '0' + str_hour
-    if len(str_minute) == 1:
-        str_minute = '0' + str_minute
-    if len(str_second) == 1:
-        str_second = '0' + str_second
-
-    print(str_hour + ':' + str_minute + ':' + str_second)
+    minute = (n // 60) % 60
+    second = n % 60
+    print('{:02d}:{:02d}:{:02d}'.format(hour, minute, second))
 
 
 if __name__ == '__main__':
