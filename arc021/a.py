ba30d4edf6862e6bfbf2911fe7055433cf8949e7
Kato Hiroki <k.hiro1818@gmail.com>
2018-09-26 22:06:05 +0900

    Initial commit.

--- arc021/a.py
+++ arc021/a.py
@@ -2,7 +2,21 @@
 
 
 def main():
-    pass
+    a = [list(map(int, input().split())) for _ in range(4)]
+
+    for i in range(4):
+        for j in range(3):
+            if a[i][j] == a[i][j + 1]:
+                print('CONTINUE')
+                exit()
+
+    for x in range(4):
+        for y in range(3):
+            if a[y][x] == a[y + 1][x]:
+                print('CONTINUE')
+                exit()
+
+    print('GAMEOVER')
 
 
 if __name__ == '__main__':
