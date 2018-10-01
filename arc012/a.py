0a00ff7490820414915298c0827a309be128da13
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-01 12:38:57 +0900

    Initial commit.

---
+++ arc012/a.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    day = input()
+
+    if day == 'Monday':
+        print(5)
+    elif day == 'Tuesday':
+        print(4)
+    elif day == 'Wednesday':
+        print(3)
+    elif day == 'Thursday':
+        print(2)
+    elif day == 'Friday':
+        print(1)
+    else:
+        print(0)
+
+
+if __name__ == '__main__':
+    main()
