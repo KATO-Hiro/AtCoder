64ac90fd89101c759b3efe6b4887a99092e59038
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-04 11:23:30 +0900

    Refactoring ARC008 task A.

--- arc008/a.py
+++ arc008/a.py
@@ -4,14 +4,11 @@
 def main():
     n = int(input())
 
-    if n <= 6:
-        print(n * 15)
-    elif (n >= 10) and (1 <= n % 10 <= 6):
-        print(n // 10 * 100 + (n % 10) * 15)
-    elif n % 10 == 0:
-        print((n // 10) * 100)
-    else:
-        print((n // 10 + 1) * 100)
+    # See:
+    # https://beta.atcoder.jp/contests/arc008/submissions/1556856
+    ans = n // 10 * 100
+    ans += min(100, n % 10 * 15)
+    print(ans)
 
 
 if __name__ == '__main__':
