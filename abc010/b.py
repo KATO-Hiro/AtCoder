671383df5ae5637dd57a579c074d475f3d217007
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-06 00:22:04 +0900

    Initial commit.

---
+++ abc010/b.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+    like_numbers = [9, 7, 3, 1]
+    count = 0
+
+    for ai in a:
+        for like_number in like_numbers:
+            if ai >= like_number:
+                count += ai - like_number
+                break
+
+    print(count)
+
+
+
+if __name__ == '__main__':
+    main()
