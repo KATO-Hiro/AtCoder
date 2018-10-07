f0970034df43af747643a0ef58bbda8a52678ae8
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-07 22:50:41 +0900

    Refactoring ABC112 task B.

--- abc112/b.py
+++ abc112/b.py
@@ -4,17 +4,14 @@
 def main():
     n, large_t = map(int, input().split())
     cost = 10000
-    count = 0
 
     for i in range(n):
         ci, ti = map(int, input().split())
 
         if ti <= large_t:
             cost = min(cost, ci)
-        else:
-            count += 1
 
-    if count != n:
+    if cost != 10000:
         print(cost)
     else:
         print('TLE')
