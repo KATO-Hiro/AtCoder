c6455b0f700027c572777337ddca0c074c981d88
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-09 22:22:45 +0900

    Refactoring ABC111 task C.

--- abc111/c.py
+++ abc111/c.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 
 
+def count_rewrite_times(n, v, even, odd):
+    ans = 0
+
+    for x in range(n):
+        if x % 2 == 0:
+            if v[x] != even:
+                ans += 1
+        else:
+            if v[x] != odd:
+                ans += 1
+
+    return ans
+
+
 def main():
     from collections import Counter
 
@@ -13,42 +27,16 @@ def main():
     else:
         even = Counter(v[::2]).most_common(1)[0][0]
         odd = Counter(v[1::2]).most_common(1)[0][0]
-        ans1 = 0
 
         if even == odd:
             even2 = Counter(v[::2]).most_common(2)[1][0]
             odd2 = Counter(v[1::2]).most_common(2)[1][0]
-            ans2 = 0
-            ans3 = 0
 
-            for x in range(n):
-                if x % 2 == 0:
-                    if v[x] != even:
-                        ans2 += 1
-                else:
-                    if v[x] != odd2:
-                        ans2 += 1
-
-            for y in range(n):
-                if y % 2 == 0:
-                    if v[y] != even2:
-                        ans3 += 1
-                else:
-                    if v[y] != odd:
-                        ans3 += 1
-
-            print(min(ans2, ans3))
+            print(min(count_rewrite_times(n, v, even, odd2),
+                      count_rewrite_times(n, v, even2, odd)))
             exit()
 
-        for i in range(n):
-            if i % 2 == 0:
-                if v[i] != even:
-                    ans1 += 1
-            else:
-                if v[i] != odd:
-                    ans1 += 1
-
-        print(ans1)
+        print(count_rewrite_times(n, v, even, odd))
 
 
 if __name__ == '__main__':
