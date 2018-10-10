cf0c0ba53c86af2fe4c9cc10d931b108fbbd274a
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-10 19:27:28 +0900

    Refactoring ABC110 task B.

--- abc110/b.py
+++ abc110/b.py
@@ -3,27 +3,16 @@
 
 def main():
     n, m, x, y = map(int, input().split())
-    xs = list(map(int, input().split()))
-    ys = list(map(int, input().split()))
+    xs = list(map(int, input().split())) + [x]
+    ys = list(map(int, input().split())) + [y]
 
-    x_max = max(xs) + 1
+    x_max = max(xs)
     y_min = min(ys)
 
-    if x_max > y_min:
+    if x_max < y_min:
+        print('No War')
+    else:
         print('War')
-        exit()
-
-    if x_max == y_min:
-        if x < x_max <= y:
-            print('No War')
-            exit()
-
-    for z in range(x_max, y_min + 1):
-        if x < z <= y:
-            print('No War')
-            exit()
-
-    print('War')
 
 
 if __name__ == '__main__':
