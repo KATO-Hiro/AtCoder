32abd82a5fcddb91b92946f77b1211ef56b2a0d3
Kato Hiroki <k.hiro1818@gmail.com>
2018-10-26 12:24:24 +0900

    Refactoring ABC007 task B.

--- abc007/b.py
+++ abc007/b.py
@@ -2,21 +2,14 @@
 
 
 def main():
-    from string import ascii_lowercase
-
     a = input()
-    a_len = len(a)
 
-    if a_len >= 2:
-        print(a[:a_len - 1])
+    # See:
+    # https://www.slideshare.net/chokudai/abc007
+    if a == 'a':
+        print('-1')
     else:
-        alpha = ascii_lowercase
-        index = alpha.index(a)
-
-        if index == 0:
-            print('-1')
-        else:
-            print(alpha[index - 1])
+        print('a')
 
 
 if __name__ == '__main__':
