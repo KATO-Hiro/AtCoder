5e33226eda21f932ed8970b69619c900ce2bb1a1
Kato Hiroki <k.hiro1818@gmail.com>
2018-11-06 12:41:12 +0900

    Initial commit.

---
+++ mujin-pc-2016/a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    c = input()
+
+    if c == 'O' or c == 'P' or c == 'K' or c == 'L':
+        print('Right')
+    else:
+        print('Left')
+
+
+if __name__ == '__main__':
+    main()
