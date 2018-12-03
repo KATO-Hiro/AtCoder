acdf6e26b2778aa0f0ddb121547ee7c87b9e73f8
Kato Hiroki <k.hiro1818@gmail.com>
2018-12-03 21:41:07 +0900

    Initial commit.

---
+++ abc114/a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    x = int(input())
+
+    if x == 3 or x == 5 or x == 7:
+        print('YES')
+    else:
+        print('NO')
+
+
+if __name__ == '__main__':
+    main()
