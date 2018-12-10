a06f3c50880047594d936a4f6f421178766388d2
Kato Hiroki <k.hiro1818@gmail.com>
2018-12-10 12:46:24 +0900

    Initial commit.

---
+++ abc115/a.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    d = int(input())
+
+    if d == 25:
+        print('Christmas')
+    elif d == 24:
+        print('Christmas Eve')
+    elif d == 23:
+        print('Christmas Eve Eve')
+    elif d == 22:
+        print('Christmas Eve Eve Eve')
+
+
+if __name__ == '__main__':
+    main()
