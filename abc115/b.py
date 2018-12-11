d6b02c7606051ecdeb0a546051a5c2686511f1f8
Kato Hiroki <k.hiro1818@gmail.com>
2018-12-11 20:37:33 +0900

    Refactoring ABC115 task B.

--- abc115/b.py
+++ abc115/b.py
@@ -4,15 +4,10 @@
 def main():
     n = int(input())
     p = sorted([int(input()) for _ in range(n)])
-    ans = 0
 
-    for index, pi in enumerate(p, 1):
-        if index == n:
-            ans += int(pi / 2)
-        else:
-            ans += pi
-
-    print(ans)
+    # See:
+    # https://img.atcoder.jp/abc115/editorial.pdf
+    print(sum(p) - max(p) // 2)
 
 
 if __name__ == '__main__':
