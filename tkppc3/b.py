9644eacf3059683c3c2c7e9031eeb71f3cc20562
Kato Hiroki <k.hiro1818@gmail.com>
2018-12-29 22:11:30 +0900

    Initial commit.

---
+++ tkppc3/b.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+
+    if n % 6 == 0:
+        print('yES')
+    else:
+        print('nO')
+
+    if n % 11 == 0:
+        print('yES')
+    else:
+        print('nO')
+
+
+if __name__ == '__main__':
+    main()
