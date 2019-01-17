c1a817865e12a416e258ac766888bc427c87b866
Kato Hiroki <k.hiro1818@gmail.com>
2019-01-17 12:28:25 +0900

    Initial commit.

---
+++ aising2019/a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+    h = int(input())
+    w = int(input())
+
+    print((n - w + 1) * (n - h + 1))
+
+
+if __name__ == '__main__':
+    main()
