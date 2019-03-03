0902796c1feaff0645565fe6fd0dc66d1a4532c6
Kato Hiroki <k.hiro1818@gmail.com>
2019-03-03 15:32:56 +0900

    Initial commit.

---
+++ abc119/c.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n, a, b, c = map(int, input().split())
+    l = [int(input()) for _ in range(n)]
+
+    # See:
+    # https://img.atcoder.jp/abc119/editorial.pdf
+    # http://drken1215.hatenablog.com/entry/2019/02/24/224100?_ga=2.107425799.427838713.1551485650-1201012407.1527836447#fnref:1
+    def dfs(i, cur_a, cur_b, cur_c):
+        if i == n:
+            if cur_a == 0 or cur_b == 0 or cur_c == 0:
+                return float('inf')
+            return abs(a - cur_a) + abs(b - cur_b) + abs(c - cur_c) - 30
+
+        result0 = dfs(i + 1, cur_a, cur_b, cur_c)
+        result1 = dfs(i + 1, cur_a + l[i], cur_b, cur_c) + 10
+        result2 = dfs(i + 1, cur_a, cur_b + l[i], cur_c) + 10
+        result3 = dfs(i + 1, cur_a, cur_b, cur_c + l[i]) + 10
+
+        return min(result0, result1, result2, result3)
+
+    print(dfs(0, 0, 0, 0))
+
+
+if __name__ == '__main__':
+    main()
