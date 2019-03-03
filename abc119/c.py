a8d85b7a4def0dc02891e9cd8439249d89a93326
Kato Hiroki <k.hiro1818@gmail.com>
2019-03-03 17:26:22 +0900

    Refactoring ABC 119 task C.

--- abc119/c.py
+++ abc119/c.py
@@ -8,18 +8,19 @@ def main():
     # See:
     # https://img.atcoder.jp/abc119/editorial.pdf
     # http://drken1215.hatenablog.com/entry/2019/02/24/224100?_ga=2.107425799.427838713.1551485650-1201012407.1527836447#fnref:1
+    # https://atcoder.jp/contests/abc119/submissions/4372229
     def dfs(i, cur_a, cur_b, cur_c):
         if i == n:
             if cur_a == 0 or cur_b == 0 or cur_c == 0:
                 return float('inf')
-            return abs(a - cur_a) + abs(b - cur_b) + abs(c - cur_c) - 30
+            return abs(a - cur_a) + abs(b - cur_b) + abs(c - cur_c)
 
-        result0 = dfs(i + 1, cur_a, cur_b, cur_c)
-        result1 = dfs(i + 1, cur_a + l[i], cur_b, cur_c) + 10
-        result2 = dfs(i + 1, cur_a, cur_b + l[i], cur_c) + 10
-        result3 = dfs(i + 1, cur_a, cur_b, cur_c + l[i]) + 10
+        w = dfs(i + 1, cur_a, cur_b, cur_c)
+        x = dfs(i + 1, cur_a + l[i], cur_b, cur_c) + 10 * (cur_a != 0)
+        y = dfs(i + 1, cur_a, cur_b + l[i], cur_c) + 10 * (cur_b != 0)
+        z = dfs(i + 1, cur_a, cur_b, cur_c + l[i]) + 10 * (cur_c != 0)
 
-        return min(result0, result1, result2, result3)
+        return min(w, x, y, z)
 
     print(dfs(0, 0, 0, 0))
 
