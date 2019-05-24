9eb5a71be6ab97ce5d3d541e440e4f162a686c86
Kato Hiroki <k.hiro1818@gmail.com>
2019-05-25 08:39:20 +0900

    Refactoring ABC126 task D

--- abc126/d.py
+++ abc126/d.py
@@ -5,19 +5,17 @@ def main():
     from collections import deque
 
     n = int(input())
-    to = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
+    graph = [[] for _ in range(n)]
 
     # See:
     # https://www.youtube.com/watch?v=26AWkQNRb-A
+    # https://atcoder.jp/contests/abc126/submissions/5456359
     for i in range(n - 1):
         ui, vi, wi = map(int, input().split())
         ui -= 1
         vi -= 1
-        to[ui].append(vi)
-        to[vi].append(ui)
-        cost[ui].append(wi)
-        cost[vi].append(wi)
+        graph[ui].append((vi, wi))
+        graph[vi].append((ui, wi))
 
     ans = [-1 for _ in range(n)]
     q = deque()
@@ -27,18 +25,14 @@ def main():
     while q:
         vj = q.popleft()
 
-        for j in range(len(to[vj])):
-            uj = to[vj][j]
-            wj = cost[vj][j]
-
+        for uj, wj in graph[vj]:
             if ans[uj] != -1:
                 continue
 
             ans[uj] = (ans[vj] + wj) % 2
             q.append(uj)
 
-    for a in ans:
-        print(a)
+    print('\n'.join(map(str, ans)))
 
 
 if __name__ == '__main__':
