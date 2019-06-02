e8e5b22b73f20074cfcb60524f1b07f547cf5762
Kato Hiroki <k.hiro1818@gmail.com>
2019-06-02 18:57:43 +0900

    Solve chokudai speedrun 002 task D

---
+++ chokudai_S002/d.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+    ans = 0
+
+    for i in range(n):
+        ai, bi = map(int, input().split())
+        ans += max(ai, bi)
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
