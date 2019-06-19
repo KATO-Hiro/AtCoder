4216c676d2f25cdc78741f2c23dae8751642349f
Kato Hiroki <k.hiro1818@gmail.com>
2019-06-19 21:26:39 +0900

    Refactoring ABC109 task D

--- abc109/d.py
+++ abc109/d.py
@@ -5,45 +5,21 @@ def main():
     h, w = map(int, input().split())
     a = [list(map(int, input().split())) for _ in range(h)]
     operations = list()
-    row = 0
-    prev_x, prev_y = 0, 0
-    odd_count = 0
 
-    while row < h:
-        if row % 2 == 0:
-            for col in range(w):
-                if a[row][col] % 2 == 1:
-                    if odd_count == 0:
-                        prev_x, prev_y = col + 1, row + 1
-                        odd_count = 1
-                    else:
-                        cur_x, cur_y = col + 1, row + 1
-                        operations.append((prev_y, prev_x, cur_y, cur_x))
-                        prev_x, prev_y = cur_x, cur_y
-                        odd_count = 0
-                else:
-                    if odd_count == 1:
-                        cur_x, cur_y = col + 1, row + 1
-                        operations.append((prev_y, prev_x, cur_y, cur_x))
-                        prev_x, prev_y = cur_x, cur_y
-        else:
-            for col in range(w - 1, -1, -1):
-                if a[row][col] % 2 == 1:
-                    if odd_count == 0:
-                        prev_x, prev_y = col + 1, row + 1
-                        odd_count = 1
-                    else:
-                        cur_x, cur_y = col + 1, row + 1
-                        operations.append((prev_y, prev_x, cur_y, cur_x))
-                        prev_x, prev_y = cur_x, cur_y
-                        odd_count = 0
-                else:
-                    if odd_count == 1:
-                        cur_x, cur_y = col + 1, row + 1
-                        operations.append((prev_y, prev_x, cur_y, cur_x))
-                        prev_x, prev_y = cur_x, cur_y
+    # See:
+    # https://atcoder.jp/contests/abc109/submissions/3154532
+    for row in range(h):
+        for col in range(w - 1):
+            if a[row][col] % 2 == 1:
+                operations.append((row + 1, col + 1, row + 1, col + 2))
+                a[row][col] -= 1
+                a[row][col + 1] += 1
 
-        row += 1
+    for row in range(h - 1):
+        if a[row][w - 1] % 2 == 1:
+            operations.append((row + 1, w, row + 2, w))
+            a[row][w - 1] -= 1
+            a[row + 1][w - 1] += 1
 
     n = len(operations)
     print(n)
