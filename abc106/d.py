82fdf31b17cd9343628e17a15440cb1ece6eb00c
Kato Hiroki <k.hiro1818@gmail.com>
2019-06-29 07:45:05 +0900

    Refactoring ABC106 task D

--- abc106/d.py
+++ abc106/d.py
@@ -3,26 +3,23 @@
 
 def main():
     n, m, q = map(int, input().split())
-    lr = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-    c = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+    summed = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 
     for _ in range(m):
         li, ri = map(int, input().split())
-        lr[li][ri] += 1
+        summed[li][ri] += 1
 
-    for i in range(n + 1):
-        for j in range(n + 1):
-            c[i][j] = c[i][j - 1] + lr[i][j]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            summed[i][j] += summed[i - 1][j]
+            summed[i][j] += summed[i][j - 1]
+            summed[i][j] -= summed[i - 1][j - 1]
 
     ans = list()
 
     for _ in range(q):
         pi, qi = map(int, input().split())
-        count = 0
-
-        for i in range(pi, qi + 1):
-            count += c[i][qi] - c[i][pi - 1]
-
+        count = summed[qi][qi] - summed[pi - 1][qi] - summed[qi][pi - 1] + summed[pi - 1][pi - 1]
         ans.append(count)
 
     print('\n'.join(map(str, ans)))
