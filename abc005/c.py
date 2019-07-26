3fd165d5610e9f291dea19b76db49811b4447469
Kato Hiroki <k.hiro1818@gmail.com>
2019-07-26 22:45:13 +0900

    Initial commit

---
+++ abc005/c.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    t = int(input())
+    n = int(input())
+    a = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
+    count = 0
+
+    if n < m:
+        print('no')
+        exit()
+
+    i = 0
+    j = 0
+
+    while i < n:
+        if j >= m:
+            break
+
+        if a[i] <= b[j] <= a[i] + t:
+            j += 1
+            count += 1
+
+        i += 1
+
+    if count >= m:
+        print('yes')
+    else:
+        print('no')
+
+
+if __name__ == '__main__':
+    main()
