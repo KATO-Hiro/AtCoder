5b7a9753c38169c547fcc812c2a3e8ac8b4e3076
Kato Hiroki <k.hiro1818@gmail.com>
2019-08-06 21:47:14 +0900

    refactoring ABC136 task B using count_digit()

--- abc136/b.py
+++ abc136/b.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
 
+def count_digit(n: int) -> int:
+    digit = 0
+
+    while n:
+        digit += 1
+        n //= 10
+
+    return digit
+
+
 def main():
     n = int(input())
     ans = 0
 
     for i in range(1, n + 1):
-        if len(list(str(i))) % 2 == 1:
+        if count_digit(i) % 2 == 1:
             ans += 1
 
     print(ans)
