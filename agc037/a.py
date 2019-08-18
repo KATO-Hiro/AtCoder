8f74a88d7d750cc0c2d2e6b4fbf1e57ab9001882
Kato Hiroki <k.hiro1818@gmail.com>
2019-08-18 21:22:34 +0900

    Initial commit

--- agc037/a.py
+++ agc037/a.py
@@ -4,20 +4,27 @@
 def main():
     s = input()
     n = len(s)
-    pre = s[0]
-    ans = n
-    i = 1
+    pre = ''
+    ans = 0
+    i = 0
 
+    # See:
+    # https://atcoder.jp/contests/agc037/submissions/6955216
     while i < n:
         cur = s[i]
 
         if pre == cur:
             if i + 1 < n:
                 pre = cur + s[i + 1]
-                ans -= 1
+                # 1文字増やすことに伴い，参照するインデックスも修正
+                i += 1
+            # 末尾の処理
+            else:
+                break
         else:
             pre = cur
 
+        ans += 1
         i += 1
 
     print(ans)
