809dfcc79b927da47dc3b983a21b1b9a827afea1
Kato Hiroki <k.hiro1818@gmail.com>
2019-08-21 22:27:38 +0900

    Initial commit

---
+++ abc138/a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    a = int(input())
+    s = input()
+
+    if a >= 3200:
+        print(s)
+    else:
+        print('red')
+
+
+if __name__ == '__main__':
+    main()
