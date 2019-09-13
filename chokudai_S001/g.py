d37600debbf08f6243ecb40f1e584f16560a634d
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-13 21:24:23 +0900

    Refactoring chokudai speed run 001

--- chokudai_S001/g.py
+++ chokudai_S001/g.py
@@ -5,7 +5,18 @@ def main():
     n = int(input())
     a = list(map(int, input().split()))
     mod = 10 ** 9 + 7
-    print(int(''.join(map(str, a))) % mod)
+    ans = 0
+
+    # KeyInsight
+    # 左側の桁から順番にmodを取る
+    # 新しい桁を見るときは，その桁数の分だけずらす
+    # See:
+    # https://atcoder.jp/contests/chokudai_S001/submissions/1458821
+    for i in range(n):
+        ans = ans * 10 ** len(str(a[i])) + a[i]
+        ans %= mod
+
+    print(ans)
 
 
 if __name__ == '__main__':
