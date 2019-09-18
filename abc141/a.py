fe61805a772df1ad2dd77353813887f956bc3fc5
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-18 21:55:10 +0900

    Initial commit

---
+++ abc141/a.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s = input()
+
+    if s == 'Sunny':
+        print('Cloudy')
+    elif s == 'Cloudy':
+        print('Rainy')
+    else:
+        print('Sunny')
+
+
+if __name__ == '__main__':
+    main()
