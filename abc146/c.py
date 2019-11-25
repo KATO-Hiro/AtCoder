697a74d23f5034327f4a7ab0ab9af00ee22e580e
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-25 22:45:56 +0900

    Refactoring ABC146 task C

--- abc146/c.py
+++ abc146/c.py
@@ -4,18 +4,27 @@
 def main():
     a, b, x = map(int, input().split())
 
-    digit = len(list(str(x // a)))
-    ans = 0
+    # KeyInsight
+    # 二分探索
+    # 単調増加であるときに，特定の値を高速に調べることができる
+    # See:
+    # https://atcoder.jp/contests/abc146/submissions/8608718
 
-    for d in range(digit, 0, -1):
-        candidate = (x - b * d) // a
-        candidate = min(candidate, 10 ** 9)
-        candidate = max(0, candidate)
+    # 両端の値は必ず条件を満たす・満たさないものを初期値として与える
+    left, right = 0, 10 ** 9 + 1
 
-        if a * candidate + b * len(list(str(candidate))) <= x:
-            ans = max(ans, candidate)
+    def can_buy(value):
+        return a * value + b * len(list(str(value))) <= x
 
-    print(ans)
+    while right - left > 1:
+        mid = (left + right) // 2
+
+        if can_buy(mid):
+            left = mid
+        else:
+            right = mid
+
+    print(left)
 
 
 if __name__ == '__main__':
