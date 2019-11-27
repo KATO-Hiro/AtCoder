d515f9b0de4a69c4afb867b064038850b927f07a
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-27 22:27:36 +0900

    Solve ABC146 task A

---
+++ abc146/a.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s = input()
+
+    if s == "SUN":
+        print(7)
+    elif s == "MON":
+        print(6)
+    elif s == "TUE":
+        print(5)
+    elif s == "WED":
+        print(4)
+    elif s == "THU":
+        print(3)
+    elif s == "FRI":
+        print(2)
+    elif s == "SAT":
+        print(1)
+
+
+if __name__ == '__main__':
+    main()
