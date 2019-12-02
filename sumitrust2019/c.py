63bb38c993e77505765babbdd3fe180dd196855d
Kato Hiroki <k.hiro1818@gmail.com>
2019-12-02 20:39:06 +0900

    Refactoring sumitrust 2019 task C

--- sumitrust2019/c.py
+++ sumitrust2019/c.py
@@ -4,13 +4,21 @@
 def main():
     x = int(input())
 
-    if x < 100:
-        print(0)
-        exit()
+    # dpによる解法
+    # See:
+    # https://atcoder.jp/contests/sumitrust2019/submissions/8726444
+    dp = [False for _ in range(x + 200)]  # 配列を大きめに確保
+    dp[0] = True  # 初期化
 
-    p, q = divmod(x, 100)
+    for i in range(x + 10):
+        if not dp[i]:
+            continue
 
-    if q <= p * 5:
+        # ある値に+100～+105できるとき
+        for j in range(100, 106):
+            dp[i + j] = True
+
+    if dp[x]:
         print(1)
     else:
         print(0)
