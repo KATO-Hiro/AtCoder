0a2f5ace2a19595270cd762c0cf4c8d1bc8da28e
Kato Hiroki <k.hiro1818@gmail.com>
2020-01-07 20:35:07 +0900

    Initial commit

--- past201912-open/f.py
+++ past201912-open/f.py
@@ -2,7 +2,27 @@
 
 
 def main():
-    pass
+    s = input()
+    upper_count = 0
+    word = ''
+    words = list()
+
+    for si in s:
+        # 探索する文字が大文字か判定
+        if si.isupper():
+            upper_count += 1
+
+        word += si
+
+        # 問題文通り、大文字2個で区切る
+        if upper_count == 2:
+            words.append(word)
+            upper_count = 0
+            word = ''
+
+    # 組み込み関数で、大文字と小文字を区別せずに比較する
+    # https://docs.python.org/ja/3/howto/sorting.html
+    print(''.join(sorted(words, key=str.lower)))
 
 
 if __name__ == '__main__':
