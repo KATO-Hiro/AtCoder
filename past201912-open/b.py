2415c346763c0c8d64badb5405d8a10136145dbd
Kato Hiroki <k.hiro1818@gmail.com>
2020-01-10 23:03:03 +0900

    Initial commit

--- past201912-open/b.py
+++ past201912-open/b.py
@@ -2,7 +2,16 @@
 
 
 def main():
-    pass
+    n = int(input())
+    a = [int(input()) for _ in range(n)]
+
+    for b, c in zip(a, a[1:]):
+        if c == b:
+            print('stay')
+        elif c > b:
+            print('up', c - b)
+        else:
+            print('down', b - c)
 
 
 if __name__ == '__main__':
