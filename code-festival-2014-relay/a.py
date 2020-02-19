7ae9737c8cd1adb050e697056ea58cc12f0864f0
Kato Hiroki <k.hiro1818@gmail.com>
2020-02-19 21:55:08 +0900

    Initial commit

---
+++ code-festival-2014-relay/a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+
+    # 素数に含まれる偶数は2のみ
+    if n == 1:
+        print(0)
+    else:
+        print(1)
+
+
+if __name__ == '__main__':
+    main()
