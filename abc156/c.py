ce1daa9444758fe62996f3a0f51f458807394f69
Kato Hiroki <k.hiro1818@gmail.com>
2020-02-23 21:28:46 +0900

    Refactoring ABC156 task C

--- abc156/c.py
+++ abc156/c.py
@@ -6,7 +6,13 @@ def main():
     x = list(map(int, input().split()))
     ans = float('inf')
 
-    for p in range(-100, 201):
+    # KeyInsight:
+    # xの座標の最小値から最大値までを探索すればよい
+    # See: https://img.atcoder.jp/abc156/editorial.pdf
+    left = min(x)
+    right = max(x)
+
+    for p in range(left, right + 1):
         summed = 0
 
         for xi in x:
