a45a2b34ce03a4a4b6171e030ecf7e2971b2bfd5
Kato Hiroki <k.hiro1818@gmail.com>
2020-02-26 21:42:27 +0900

    Refactoring ABC146 task D

--- abc146/d.py
+++ abc146/d.py
@@ -2,6 +2,7 @@
 
 
 def main():
+    # 再帰の上限回数のデフォルトを変更
     from sys import setrecursionlimit
     setrecursionlimit(10 ** 7)
 
@@ -20,13 +21,21 @@ def main():
         graph[ai].append((bi, i))
         graph[bi].append((ai, i))
 
+    # 実装できなかったところ
+    # 1. 色のカウントをどう管理するか?
+    # 2. 辺のidをどう管理するか?
+
+    # 親方向からの色番号を引数に持つ
     def dfs(vertex, parent=-1, edge_color=-1):
+        # 一般的なポイント: 頂点に関する処理を書く
         color_code = 1
 
+        # 基本形: 親の頂点周りを探索
         for to, edge_id in graph[vertex]:
             if to == parent:
                 continue
 
+            # 一般的なポイント: 辺に関する処理を書く
             if color_code == edge_color:
                 color_code += 1
 
@@ -34,6 +43,9 @@ def main():
             color_code += 1
 
             dfs(to, vertex, ans[edge_id])
+            # 一般的なポイント: 辺に関する処理を書く
+
+        # 一般的なポイント: 頂点に関する処理を書く
 
     dfs(0)
     count = 0
