c850dc5f44fbb83d951d188238de6d6ec6dac2fe
Kato Hiroki <k.hiro1818@gmail.com>
2020-03-23 22:40:10 +0900

    Initial commit

---
+++ abc159/c.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    l = int(input())
+    print((l / 3) ** 3)
+
+
+if __name__ == '__main__':
+    main()
