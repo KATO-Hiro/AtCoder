49726f86c33df2b0407e31bfe39f46b52157011a
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-01 22:08:03 +0900

    Initial commit

---
+++ abc160/a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s = input()
+
+    if s[2] == s[3] and s[4] == s[5]:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
