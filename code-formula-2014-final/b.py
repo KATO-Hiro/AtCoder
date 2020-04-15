ba87e9b309c85c1d529d3f9ffdcfd1f6836e86f5
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-15 21:48:20 +0900

    Initial commit

--- code-formula-2014-final/b.py
+++ code-formula-2014-final/b.py
@@ -2,8 +2,14 @@
 
 
 def main():
+    from math import ceil
     import sys
     input = sys.stdin.readline
+
+    n = int(input())
+    odd = ceil(n / 2)
+    even = n - odd
+    print(3 * odd - 2 * even)
 
 
 if __name__ == '__main__':
