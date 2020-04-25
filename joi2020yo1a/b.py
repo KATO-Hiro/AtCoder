dfea9c39dbad5227fa16d64c540979b27f8c8176
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-25 22:03:16 +0900

    Initial commit

--- joi2020yo1a/b.py
+++ joi2020yo1a/b.py
@@ -2,7 +2,15 @@
 
 
 def main():
-    pass
+    n = int(input())
+    s = input()
+    count = s.count('a')
+    count += s.count('i')
+    count += s.count('u')
+    count += s.count('e')
+    count += s.count('o')
+
+    print(count)
 
 
 if __name__ == '__main__':
