43726dfd4f78e7db91040bf2560c562837fc0ec6
Kato Hiroki <k.hiro1818@gmail.com>
2020-05-07 22:01:54 +0900

    Initial commit

--- abc162/a.py
+++ abc162/a.py
@@ -2,8 +2,12 @@
 
 
 def main():
-    import sys
-    input = sys.stdin.readline
+    n = input()
+
+    if '7' in n:
+        print('Yes')
+    else:
+        print('No')
 
 
 if __name__ == '__main__':
