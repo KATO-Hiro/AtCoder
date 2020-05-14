dcf96586a3772a004438f81d3c49a5f17a9d76ce
Kato Hiroki <k.hiro1818@gmail.com>
2020-05-14 23:00:25 +0900

    Initial commit

---
+++ abc167/a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s = input()
+    t = input()
+
+    if s == t[:-1]:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
