8841bc6d634911b3ee5588a769be72177a357caf
Kato Hiroki <k.hiro1818@gmail.com>
2020-05-18 22:13:16 +0900

    Initial commit

---
+++ abc168/a.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = str(input())
+    digit = n[-1]
+
+    if digit == '3':
+        print('bon')
+    elif digit == '0':
+        print('pon')
+    elif digit == '1':
+        print('pon')
+    elif digit == '6':
+        print('pon')
+    elif digit == '8':
+        print('pon')
+    else:
+        print('hon')
+
+
+if __name__ == '__main__':
+    main()
