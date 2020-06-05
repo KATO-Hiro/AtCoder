9319c88410a9182e1f463100fdbae534c5ed60e5
Kato Hiroki <k.hiro1818@gmail.com>
2020-06-05 20:52:08 +0900

    Initial commit

--- abc166/d.py
+++ abc166/d.py
@@ -5,6 +5,18 @@ def main():
     import sys
     input = sys.stdin.readline
 
+    x = int(input())
+
+    # KeyInsight:
+    # 範囲を絞り込んで、全探索
+    for a in range(-200, 200):
+        for b in range(-200, 200):
+            if (a ** 5) - (b ** 5) == x:
+                print(a, b)
+                exit()
+
+    print(-1)
+
 
 if __name__ == '__main__':
     main()
