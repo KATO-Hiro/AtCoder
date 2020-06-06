a052a4348e7045eb2434d20ca3f643430023554f
Kato Hiroki <k.hiro1818@gmail.com>
2020-06-06 22:26:29 +0900

    Initial commit

---
+++ abc169/a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    a, b = map(int, input().split())
+    print(a * b)
+
+
+if __name__ == '__main__':
+    main()
