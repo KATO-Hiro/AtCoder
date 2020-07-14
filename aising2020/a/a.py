5c5309509735f0097160eee835eb7d4ee8798df4
Kato Hiroki <k.hiro1818@gmail.com>
2020-07-14 11:49:11 +0000

    Initial commit

--- aising2020/a/a.py
+++ aising2020/a/a.py
@@ -5,6 +5,15 @@ def main():
     import sys
     input = sys.stdin.readline
 
+    l, r, d = map(int, input().split())
+    count = 0
+
+    for i in range(l, r + 1):
+        if i % d == 0:
+            count += 1
+
+    print(count)
+
 
 if __name__ == '__main__':
     main()
