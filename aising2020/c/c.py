bb5a5f001fb026bf380e0fa3324fe0705e5fedda
Kato Hiroki <k.hiro1818@gmail.com>
2020-07-15 12:23:01 +0000

    Initial commit

--- aising2020/c/c.py
+++ aising2020/c/c.py
@@ -2,21 +2,28 @@
 
 
 def main():
-    from math import sqrt
     import sys
     input = sys.stdin.readline
 
-    # n = int(input())
-    n = 10 ** 4
-    m = int(sqrt(n))
-    count = 0
+    n = int(input())
+    ans = [0 for _ in range(10 ** 6 + 2000)]
+    candidate = 110
+
+    # KeyInsight:
+    # ◯: 範囲を限定して全探索
+    # ×: 最大ケースに、それ未満のケースが含まれる場合は、最大ケースだけ考える
+    for x in range(1, candidate + 1):
+        for y in range(1, candidate + 1):
+            for z in range(1, candidate + 1):
+                total = 0
+                total += (x ** 2) + (y ** 2) + (z ** 2)
+                total += (x * y) + (y * z) + (z * x)
+
+                if total < (10 ** 6 + 2000):
+                    ans[total] += 1
 
     for i in range(1, n + 1):
-        for x in range(1, m + 1):
-            for y in range(m + 1, 0, -1):
-                count += 1
-
-    print(count)
+        print(ans[i])
 
 
 if __name__ == '__main__':
