4214d31d728e9dba0cf1eb5907546e1225e53bb8
Kato Hiroki <k.hiro1818@gmail.com>
2020-08-07 14:05:32 +0000

    Initial commit

---
+++ abc174/a/a.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    x = int(input())
+
+    if x >= 30:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
