4c416743a26b399d5e529cbd35ac5a0e661a9b9d
Kato Hiroki <k.hiro1818@gmail.com>
2020-08-09 22:24:45 +0900

    Initial commit

--- tokiomarine2020/b.py
+++ tokiomarine2020/b.py
@@ -5,6 +5,25 @@ def main():
     import sys
     input = sys.stdin.readline
 
+    a, v = map(int, input().split())
+    b, w = map(int, input().split())
+    t = int(input())
+
+    if v == w:
+        print('NO')
+        exit()
+
+    if a <= b:
+        if 0 < ((b - a) / (v - w)) <= t:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        if 0 < ((b - a) / (w - v)) <= t:
+            print('YES')
+        else:
+            print('NO')
+
 
 if __name__ == '__main__':
     main()
