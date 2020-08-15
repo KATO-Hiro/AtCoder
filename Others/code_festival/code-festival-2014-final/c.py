c6856f49781b4f26c00af392658b7139b26a9439
Kato Hiroki <k.hiro1818@gmail.com>
2020-08-15 22:25:56 +0900

    Initial commit

--- Others/code_festival/code-festival-2014-final/c.py
+++ Others/code_festival/code-festival-2014-final/c.py
@@ -1,8 +1,42 @@
 # -*- coding: utf-8 -*-
 
 
+def convert_decimal_to_n_ary_number(n: int, m_ary_number: int = 2) -> int:
+    '''Represents conversion from decimal number to n-ary number.
+    Args:
+        n: Input number (greater than 1).
+        m_ary_number: m-ary number (from 2 to 10).
+    Returns:
+        values of m-ary number.
+    Landau notation: O(log n)
+    '''
+    if n < m_ary_number:
+        return n
+
+    ans = ''
+
+    while n >= m_ary_number:
+        p, q = divmod(n, m_ary_number)
+        n = p
+        ans += str(q)
+
+        if n < m_ary_number:
+            ans += str(p)
+
+    return int(ans[::-1])
+
+
 def main():
-    pass
+    a = int(input())
+
+    for i in range(10, 10 ** 4 + 1):
+        result = convert_decimal_to_n_ary_number(a, i)
+
+        if result == i:
+            print(i)
+            exit()
+
+    print(-1)
 
 
 if __name__ == '__main__':
