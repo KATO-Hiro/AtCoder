649ce0f32a6cb5eb76054839af2de8e5a6c4e571
Kato Hiroki <k.hiro1818@gmail.com>
2020-08-18 23:14:29 +0900

    Initial commit

--- ABC/abc151-abc200/abc175/a/a.py
+++ ABC/abc151-abc200/abc175/a/a.py
@@ -2,8 +2,19 @@
 
 
 def main():
-    import sys
-    input = sys.stdin.readline
+    s = input()
+
+    # See:
+    # https://atcoder.jp/contests/abc175/submissions/15907467
+    # Pythonのシンタックスを活用
+    if 'RRR' in s:
+        print(3)
+    elif 'RR' in s:
+        print(2)
+    elif 'R' in s:
+        print(1)
+    else:
+        print(0)
 
 
 if __name__ == '__main__':
