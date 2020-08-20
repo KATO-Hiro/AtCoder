4b95959137ac7f7cadebaa6de103b78f4221adb2
Kato Hiroki <k.hiro1818@gmail.com>
2020-08-20 20:42:48 +0900

    Initial commit

--- ABC/abc151-abc200/abc168/c.py
+++ ABC/abc151-abc200/abc168/c.py
@@ -3,13 +3,22 @@
 
 def main():
     from math import cos
+    from math import pi
+    from math import sqrt
     import sys
     input = sys.stdin.readline
 
     a, b, h, m = map(int, input().split())
-    c = (a ** 2) + (b ** 2) - 2 * a * b * cos((5 * h - m) * 6)
-    print(c ** 0.5)
-    print(360 - ((5 * h - m) * 6))
+
+    # KeyInsight:
+    # 時針: h + m / 60時間で動いた角度 = 分針が動いた分も考慮する必要がある
+    # 余弦定理
+    hour = (60 * h + m) / 720 * 2 * pi
+    minute = m / 60 * 2 * pi
+    angle = abs(hour - minute)
+
+    c = (a ** 2) + (b ** 2) - 2 * a * b * cos(angle)
+    print(sqrt(c))
 
 
 if __name__ == '__main__':
