785b1b531b2372bb82d36c16cdf91ea498b01986
Kato Hiroki <k.hiro1818@gmail.com>
2020-09-03 20:20:57 +0900

    Solvev ABC177 task A

---
+++ ABC/abc151-abc200/abc177/a/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    d, t, s = map(int, input().split())
+
+    if s * t >= d:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
