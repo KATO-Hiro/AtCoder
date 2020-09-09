4e652ac36c4d9ef4b76ebc352dedf31614a03089
Kato Hiroki <k.hiro1818@gmail.com>
2020-09-09 21:49:07 +0900

    Solve practice 2 task A

---
+++ Others/practice/practice2/a/main.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+
+class UnionFind(object):
+    '''Represents a data structure that tracks a set of elements partitioned
+       into a number of disjoint (non-overlapping) subsets.
+    Landau notation: O(α(n)), where α(n) is the inverse Ackermann function.
+    See:
+    https://www.youtube.com/watch?v=zV3Ul2pA2Fw
+    https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    https://atcoder.jp/contests/abc120/submissions/4444942
+    '''
+
+    def __init__(self, number_count: int):
+        '''
+        Args:
+            number_count: The size of elements (greater than 2).
+        '''
+        self.parent_numbers = [-1 for _ in range(number_count)]
+
+    def find_root(self, number: int) -> int:
+        '''Follows the chain of parent pointers from number up the tree until
+           it reaches a root element, whose parent is itself.
+        Args:
+            number: The trees id (0-index).
+        Returns:
+            The index of a root element.
+        '''
+        if self.parent_numbers[number] < 0:
+            return number
+
+        self.parent_numbers[number] = self.find_root(self.parent_numbers[number])
+        return self.parent_numbers[number]
+
+    def get_group_size(self, number: int) -> int:
+        '''
+        Args:
+            number: The trees id (0-index).
+        Returns:
+            The size of group.
+        '''
+        return -self.parent_numbers[self.find_root(number)]
+
+    def is_same_group(self, number_x: int, number_y: int) -> bool:
+        '''Represents the roots of tree number_x and number_y are in the same
+           group.
+        Args:
+            number_x: The trees x (0-index).
+            number_y: The trees y (0-index).
+        '''
+        return self.find_root(number_x) == self.find_root(number_y)
+
+    def merge_if_needs(self, number_x: int, number_y: int) -> bool:
+        '''Uses find_root to determine the roots of the tree number_x and
+           number_y belong to. If the roots are distinct, the trees are combined
+           by attaching the roots of one to the root of the other.
+        Args:
+            number_x: The trees x (0-index).
+            number_y: The trees y (0-index).
+        '''
+        x = self.find_root(number_x)
+        y = self.find_root(number_y)
+
+        if x == y:
+            return False
+
+        if self.get_group_size(x) >= self.get_group_size(y):
+            self.parent_numbers[x] += self.parent_numbers[y]
+            self.parent_numbers[y] = x
+        else:
+            self.parent_numbers[y] += self.parent_numbers[x]
+            self.parent_numbers[x] = y
+        return True
+
+
+def main():
+    n, q = map(int, input().split())
+    uf = UnionFind(n)
+
+    for i in range(q):
+        ti, ui, vi = map(int, input().split())
+
+        if ti == 0:
+            uf.merge_if_needs(ui, vi)
+        else:
+            if uf.is_same_group(ui, vi):
+                print(1)
+            else:
+                print(0)
+
+
+if __name__ == '__main__':
+    main()
