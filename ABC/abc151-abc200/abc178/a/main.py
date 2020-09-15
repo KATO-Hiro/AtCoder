0c1136f6c5c3ca998d95a6acb05cffbb5450c7b7
Kato Hiroki <k.hiro1818@gmail.com>
2020-09-15 21:47:12 +0900

    Solve ABC178 task A

---
+++ ABC/abc151-abc200/abc178/a/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    x = int(input())
+
+    if x == 0:
+        print(1)
+    else:
+        print(0)
+
+
+if __name__ == '__main__':
+    main()
