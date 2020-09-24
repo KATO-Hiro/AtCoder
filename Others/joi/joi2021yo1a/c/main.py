44126d5dc8cca88189c569f1cd432acfd2446dc9
Kato Hiroki <k.hiro1818@gmail.com>
2020-09-24 22:18:10 +0900

    Solve joi 2021 yo1 task C

---
+++ Others/joi/joi2021yo1a/c/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    a = set(map(int, input().split()))
+    b = set(map(int, input().split()))
+
+    print(*sorted(a & b), sep="\n")
+
+
+if __name__ == '__main__':
+    main()
