4a32d0084196a269b32ad02b1e9573d90e53581b
Kato Hiroki <k.hiro1818@gmail.com>
2020-09-29 21:09:51 +0900

    Solve abl task C

---
+++ Others/acl/abl/c/main.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+
+import typing
+
+
+# See:
+# https://github.com/not522/ac-library-python/blob/master/atcoder/dsu.py
+class DSU:
+    '''
+    Implement (union by size) + (path compression)
+    Reference:
+    Zvi Galil and Giuseppe F. Italiano,
+    Data structures and algorithms for disjoint set union problems
+    '''
+
+    def __init__(self, n: int = 0):
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a: int, b: int) -> int:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def same(self, a: int, b: int) -> bool:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        if self.parent_or_size[a] < 0:
+            return a
+
+        self.parent_or_size[a] = self.leader(self.parent_or_size[a])
+        return self.parent_or_size[a]
+
+    def size(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self) -> typing.List[typing.List[int]]:
+        leader_buf = [self.leader(i) for i in range(self._n)]
+
+        result: typing.List[typing.List[int]] = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+
+        return list(filter(lambda r: r, result))
+
+
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    dsu = DSU(n)
+
+    for i in range(m):
+        ai, bi = map(int, input().split())
+        ai -= 1
+        bi -= 1
+        dsu.merge(ai, bi)
+
+    ans = 0
+
+    for j in range(1, n):
+        if not dsu.same(0, j):
+            dsu.merge(0, j)
+            ans += 1
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
