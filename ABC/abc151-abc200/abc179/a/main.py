58f2f7165400db2726f07b8ae14dd5ddbc7f1e34
Kato Hiroki <k.hiro1818@gmail.com>
2020-10-08 21:52:04 +0900

    Solve ABC179 task A

---
+++ ABC/abc151-abc200/abc179/a/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s = input()
+
+    if s[-1] == 's':
+        print(s + 'es')
+    else:
+        print(s + 's')
+
+
+if __name__ == '__main__':
+    main()
