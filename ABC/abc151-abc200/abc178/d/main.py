e16da047167442847204349c4cc83f2d57419a49
Kato Hiroki <k.hiro1818@gmail.com>
2020-10-09 19:59:26 +0900

    Refactoring

--- ABC/abc151-abc200/abc178/d/main.py
+++ ABC/abc151-abc200/abc178/d/main.py
@@ -9,14 +9,21 @@ def main():
     mod = 10 ** 9 + 7
     dp = [0 for _ in range(s + 1)]
     dp[0] = 1
+    total = 0
 
     # KeyInsight:
     # 数字を球が連続して並んでいる状態に言い換える
     # dp[i]: 最後に切った場所がi。両端も含める。
+    # 高速化: 累積和を保存する変数を用意 & 差分のみを更新
+
+    # See:
+    # https://www.youtube.com/watch?v=yLkJZXkB6D0&feature=youtu.be
     for i in range(1, s + 1):
-        for j in range(i - 3 + 1):
-            dp[i] += dp[j]
-            dp[i] %= mod
+        if (i - 3) >= 0:
+            total += dp[i - 3]
+
+        dp[i] += total
+        dp[i] %= mod
 
     print(dp[s])
 
