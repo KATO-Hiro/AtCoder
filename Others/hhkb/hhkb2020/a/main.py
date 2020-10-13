e16e053a4de8f2c9a81b40c70bedf0e5a9c674cb
Kato Hiroki <k.hiro1818@gmail.com>
2020-10-13 23:04:26 +0900

    Solve HHKB 2020 task A

---
+++ Others/hhkb/hhkb2020/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s = input()
+    t = input()
+
+    if s == 'Y':
+        print(t.upper())
+    else:
+        print(t)
+
+
+if __name__ == "__main__":
+    main()
