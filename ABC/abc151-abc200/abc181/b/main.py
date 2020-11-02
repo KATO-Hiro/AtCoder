02661165da2de4852ab77d38b8a187efeddefcd4
Kato Hiroki <k.hiro1818@gmail.com>
2020-11-02 22:13:57 +0900

    Refactoring

--- ABC/abc151-abc200/abc181/b/main.py
+++ ABC/abc151-abc200/abc181/b/main.py
@@ -2,19 +2,16 @@
 
 
 def main():
-    from itertools import accumulate
-
     import sys
 
     input = sys.stdin.readline
 
     n = int(input())
-    summed = list(accumulate([i for i in range(10 ** 6 + 10)]))
     ans = 0
 
     for i in range(n):
         ai, bi = map(int, input().split())
-        ans += summed[bi] - summed[ai - 1]
+        ans += (ai + bi) * (bi - ai + 1) // 2
 
     print(ans)
 
