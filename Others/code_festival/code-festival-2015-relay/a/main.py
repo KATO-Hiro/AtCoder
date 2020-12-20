6d8bc34ac414256cd4741d7a74d2c72f1f02215b
Kato Hiroki <k.hiro1818@gmail.com>
2020-12-20 22:24:37 +0900

    Refactoring (#214)

--- Others/code_festival/code-festival-2015-relay/a/main.py
+++ Others/code_festival/code-festival-2015-relay/a/main.py
@@ -7,19 +7,7 @@ def main():
     input = sys.stdin.readline
 
     t = int(input())
-    ranks = [[] for _ in range(20)]
-
-    for i in range(1, 10 + 1):
-        if i % 2 == 1:
-            for j in range(1, 20 + 1):
-                rank = 40 * (i - 1) // 2 + j
-                ranks[j - 1].append(rank)
-        else:
-            for j in range(20, 0, -1):
-                rank = 20 * i + 1 - j
-                ranks[j - 1].append(rank)
-
-    print(sum(ranks[t - 1]))
+    print(1005)
 
 
 if __name__ == "__main__":
