7f903752e71b6000fee5bbe4ba14bf8d07a26f32
Kato Hiroki <k.hiro1818@gmail.com>
2020-12-24 22:07:43 +0900

    Refactoring

--- Others/past/past202004-open/d/main.py
+++ Others/past/past202004-open/d/main.py
@@ -1,34 +1,37 @@
 # -*- coding: utf-8 -*-
 
 
+# See:
+# https://atcoder.jp/contests/past202004-open/submissions/12572084
+def calc(si):
+    ni = len(si)
+    results = set()
+
+    for i in range(1 << ni):
+        t = ""
+
+        for j in range(ni):
+            if i >> j & 1:
+                t += si[j]
+            else:
+                t += "."
+
+        results.add(t)
+
+    return results
+
+
 def main():
     s = input()
     n = len(s)
     strings = set()
-    strings.add(".")
 
-    for si in s:
-        strings.add(si)
+    for i in range(n):
+        for j in range(1, 3 + 1):
+            si = s[i : i + j]
 
-    if n >= 2:
-        strings.add("..")
-
-        for first, second in zip(s, s[1:]):
-            strings.add(first + second)
-            strings.add(first + ".")
-            strings.add("." + second)
-
-    if n >= 3:
-        strings.add("...")
-
-        for first, second, third in zip(s, s[1:], s[2:]):
-            strings.add(first + second + third)
-            strings.add(first + second + ".")
-            strings.add(first + "." + third)
-            strings.add("." + second + third)
-            strings.add(first + "." + ".")
-            strings.add("." + second + ".")
-            strings.add("." + "." + third)
+            for c in calc(si):
+                strings.add(c)
 
     print(len(strings))
 
