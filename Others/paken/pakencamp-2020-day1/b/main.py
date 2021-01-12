d41d5a98d844455ef8955dc780199b1d41300279
Kato Hiroki <k.hiro1818@gmail.com>
2021-01-12 22:48:25 +0900

    Refactoring (#303)

--- Others/paken/pakencamp-2020-day1/b/main.py
+++ Others/paken/pakencamp-2020-day1/b/main.py
@@ -2,13 +2,8 @@
 
 
 def main():
-    x = input()
-    ans = 0
-
-    for xi in x:
-        ans = max(ans, int(xi))
-
-    print(ans)
+    x = list(input())
+    print(max(x))
 
 
 if __name__ == "__main__":
