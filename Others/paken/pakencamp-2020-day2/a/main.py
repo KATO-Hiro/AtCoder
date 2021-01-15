759089c97cd504cec1240e7ff61fbe9e97828792
Kato Hiroki <k.hiro1818@gmail.com>
2021-01-15 22:58:00 +0900

    Solve pakencamp 2020 day2 task A

---
+++ Others/paken/pakencamp-2020-day2/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    a, b = map(int, input().split())
+
+    print(max(a, b), a + b)
+
+
+if __name__ == "__main__":
+    main()
