14b471ba7fe23ddca69ec124c7bd37bdbd15bb6f
Kato Hiroki <k.hiro1818@gmail.com>
2021-01-16 22:31:57 +0900

    Refactoring

--- Others/paken/pakencamp-2020-day1/f/main.py
+++ Others/paken/pakencamp-2020-day1/f/main.py
@@ -3,13 +3,19 @@
 
 def main():
     p = int(input())
-    f = [0, 1, 1]
+    f = [0 for _ in range(2 * 10 ** 5 + 100)]
+    f[1] = 1
+    f[2] = 1
 
-    for i in range(1, 10 ** 5):
-        f.append(f[-1] + f[-2])
+    if p == 1:
+        print(1)
+        exit()
 
-    for j in range(1, 10 ** 5):
-        if f[j] % p == 0:
+    for i in range(2, 2 * 10 ** 5):
+        f[i] = (f[i - 1] + f[i - 2]) % p
+
+    for j in range(1, 2 * 10 ** 5):
+        if f[j] == 0:
             print(j)
             exit()
 
