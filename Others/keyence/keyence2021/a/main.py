24e735f91713f622b2dc2a247addc414ef6325f7
Kato Hiroki <k.hiro1818@gmail.com>
2021-01-17 21:00:07 +0900

    Refactoring (#327)

--- Others/keyence/keyence2021/a/main.py
+++ Others/keyence/keyence2021/a/main.py
@@ -6,16 +6,12 @@ def main():
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
 
-    max_a = [0 for _ in range(n)]
-    max_a[0] = a[0]
-
-    for i in range(1, n):
-        max_a[i] = max(max_a[i - 1], a[i])
-
     ans = 0
+    a_max = 0
 
-    for max_ai, bi in zip(max_a, b):
-        ans = max(ans, max_ai * bi)
+    for ai, bi in zip(a, b):
+        a_max = max(a_max, ai)
+        ans = max(ans, a_max * bi)
         print(ans)
 
 
