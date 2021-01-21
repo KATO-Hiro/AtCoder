f3f8495a2d58c19759f77c2baa26b8b3cfd0f31c
Kato Hiroki <k.hiro1818@gmail.com>
2021-01-21 22:23:22 +0900

    Refactoring

--- Others/past/past202012-open/c/main.py
+++ Others/past/past202012-open/c/main.py
@@ -4,23 +4,16 @@
 def main():
     n = int(input())
     d = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    ans = ""
+    a = list()
 
-    if n < 36:
-        print(d[n])
-        exit()
+    while n:
+        a.append(n % 36)
+        n //= 36
 
-    for i in range(2, -1, -1):
-        p, q = divmod(n, 36 ** i)
-
-        if i == 2 and p == 0:
-            continue
-
-        ans += d[p]
-
-        n = q
-
-    print(ans)
+    if len(a) == 0:
+        print(0)
+    else:
+        print("".join([d[ai] for ai in a][::-1]))
 
 
 if __name__ == "__main__":
