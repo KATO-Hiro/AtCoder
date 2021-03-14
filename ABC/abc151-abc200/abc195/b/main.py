da86bc8cddf29323705e63c7ca9104d9f2c316a9
Kato Hiroki <k.hiro1818@gmail.com>
2021-03-14 16:52:24 +0900

    Refactoring (#242)

--- ABC/abc151-abc200/abc195/b/main.py
+++ ABC/abc151-abc200/abc195/b/main.py
@@ -11,10 +11,18 @@ def main():
     b = int(b)
     w = int(w) * 1000
 
-    if w % a != 0 and w % b != 0 and (w // a == w // b):
+    value_min = float("inf")
+    value_max = -float("inf")
+
+    for i in range(1, 10 ** 6 + 1):
+        if a * i <= w <= b * i:
+            value_min = min(value_min, i)
+            value_max = max(value_min, i)
+
+    if value_min == float("inf"):
         print("UNSATISFIABLE")
     else:
-        print(((w + b - 1) // b), w // a)
+        print(value_min, value_max)
 
 
 if __name__ == "__main__":
