b45b79aa188b4d6cadb88791612e85989fcc2db8
Kato Hiroki <k.hiro1818@gmail.com>
2021-05-21 22:09:23 +0900

    Refactoring (#847)

--- ARC/arc101-arc150/arc119/a/main.py
+++ ARC/arc101-arc150/arc119/a/main.py
@@ -9,24 +9,9 @@ def main():
     n = int(input())
     ans = float("inf")
 
-    if n == 1:
-        print(1)
-        exit()
-
     for b in range(62):
-        a_lower = 0
-        a_upper = n
-
-        while (a_upper - a_lower) > 1:
-            a = (a_lower + a_upper) // 2
-            c = n - a * (2 ** b)
-
-            if c >= 0:
-                a_lower = a
-
-                ans = min(ans, a + b + c)
-            else:
-                a_upper = a
+        a, c = divmod(n, 2 ** b)
+        ans = min(ans, a + b + c)
 
     print(ans)
 
