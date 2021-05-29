a7887c644621cbbec32192b5586a595657cf546b
Kato Hiroki <k.hiro1818@gmail.com>
2021-05-29 14:16:06 +0900

    Refactoring (#855)

--- ARC/arc101-arc150/arc120/a/main.py
+++ ARC/arc101-arc150/arc120/a/main.py
@@ -2,26 +2,21 @@
 
 
 def main():
-    from itertools import accumulate
-
     import sys
 
     input = sys.stdin.readline
 
     n = int(input())
     a = list(map(int, input().split()))
-    summed_a = list(accumulate([0] + a))
-    max_a = [0 for _ in range(n)]
-    max_a[0] = a[0]
 
-    for index, ai in enumerate(a[1:]):
-        max_a[index + 1] = max(max_a[index], ai)
-
-    delta = 0
+    summed, total = 0, 0
+    max_value = 0
 
     for index, ai in enumerate(a):
-        delta += summed_a[index + 1]
-        ans = delta + max_a[index] * (index + 1)
+        summed += ai
+        total += summed
+        max_value = max(max_value, ai)
+        ans = total + max_value * (index + 1)
         print(ans)
 
 
