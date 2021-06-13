08cdc5d3b36c532b0d819700c484b86932df0c45
Kato Hiroki <k.hiro1818@gmail.com>
2021-06-13 09:51:52 +0900

    Refactoring

--- ABC/abc201-abc250/abc204/d/main.py
+++ ABC/abc201-abc250/abc204/d/main.py
@@ -8,24 +8,20 @@ def main():
 
     n = int(input())
     t = list(map(int, input().split()))
-    inf = 10 ** 7
-    m = 10 ** 5 + 3000
-    dp = [[inf for _ in range(m)] for __ in range(n + 10)]
-    dp[0][0] = 0
+    m = 10 ** 5 + 1
+    dp = [False for _ in range(m)]
+    dp[0] = True
 
-    for i in range(n):
-        for j in range(101001):
-            # Use 1st
-            dp[i + 1][j + t[i]] = min(dp[i][j], dp[i + 1][j + t[i]])
-
-            # Use 2nd
-            dp[i + 1][j] = min(dp[i][j] + t[i], dp[i + 1][j])
+    for ti in t:
+        for i in reversed(range(m - ti)):
+            dp[i + ti] |= dp[i]
 
     total = sum(t)
     ans = float("inf")
 
-    for t in range(101001):
-        ans = min(ans, max(dp[n][t], total - dp[n][t]))
+    for t in range(m):
+        if dp[t]:
+            ans = min(ans, max(t, total - t))
 
     print(ans)
 
