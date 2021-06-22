f598825c81f7edca72d0927c92397b57244cde9d
Kato Hiroki <k.hiro1818@gmail.com>
2021-06-22 22:23:49 +0900

    Refatoring

--- Others/past/past202012-open/d/main.py
+++ Others/past/past202012-open/d/main.py
@@ -3,22 +3,10 @@
 
 def main():
     n = int(input())
-    d = dict()
+    s = [input().rstrip() for _ in range(n)]
 
-    for i in range(n):
-        si = input().rstrip()
-        si_int = int(si)
-        si_int_size = len(list(str(si_int)))
-        si_str_size = len(list(si))
-
-        if si_int in d.keys():
-            d[si_int].append(si_str_size - si_int_size)
-        else:
-            d[si_int] = [si_str_size - si_int_size]
-
-    for key in sorted(d.keys()):
-        for k in sorted(d[key], reverse=True):
-            print("0" * k + str(key))
+    sorted_s = sorted(s, key=lambda x: (int(x), -len(x)))
+    print("\n".join(sorted_s))
 
 
 if __name__ == "__main__":
