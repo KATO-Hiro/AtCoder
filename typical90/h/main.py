df6814c7eabda0eed5a4aa0b6f1fa4bd22b5073a
Kato Hiroki <k.hiro1818@gmail.com>
2021-07-25 14:10:38 +0900

    Refactoring

--- typical90/h/main.py
+++ typical90/h/main.py
@@ -3,24 +3,20 @@
 
 def main():
     n = int(input())
-    s = input()
+    s = list(input())
     t = list("atcoder")
     m = len(t)
     mod = 10 ** 9 + 7
-    dp = [[0 for _ in range(m + 5)] for _ in range(n + 1)]
-    dp[0][0] = 1
+    dp = [0 for _ in range(m + 1)]
+    dp[0] = 1
 
-    for i, si in enumerate(s):
-        for j in range(m + 1):
-            dp[i + 1][j] += dp[i][j]
+    for si in s:
+        for j in range(m):
+            if si == t[j]:
+                dp[j + 1] += dp[j]
+                dp[j + 1] %= mod
 
-            if si in t and t.index(si) == j:
-                dp[i + 1][j + 1] += dp[i][j]
-
-        for j in range(m + 1):
-            dp[i + 1][j] %= mod
-
-    print(dp[n][m])
+    print(dp[m])
 
 
 if __name__ == "__main__":
