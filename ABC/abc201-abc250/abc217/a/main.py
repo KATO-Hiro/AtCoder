090c2b28690a64c53089215211e7d3208c772dff
Kato Hiroki <k.hiro1818@gmail.com>
2021-09-10 06:32:16 +0900

    Solve ABC217 task A

---
+++ ABC/abc201-abc250/abc217/a/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    s, t = input().split()
+
+    if s < t:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
