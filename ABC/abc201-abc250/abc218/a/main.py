407eb2bbecbae5c3f9f710048a810549e7182695
Kato Hiroki <k.hiro1818@gmail.com>
2021-09-17 06:44:24 +0900

    Solve ABC218 task A

---
+++ ABC/abc201-abc250/abc218/a/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    n = int(input())
+    n -= 1
+    s = input()
+
+    if s[n] == 'o':
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
