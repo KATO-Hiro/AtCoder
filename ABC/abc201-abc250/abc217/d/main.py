5cd51d275f8186f03bd84440088159777a848fc0
Kato Hiroki <k.hiro1818@gmail.com>
2021-09-26 22:16:42 +0900

    Refactoring

--- ABC/abc201-abc250/abc217/d/main.py
+++ ABC/abc201-abc250/abc217/d/main.py
@@ -1,54 +1,71 @@
 # -*- coding: utf-8 -*-
 
-
-# See:
-# https://qiita.com/Kiri8128/items/6256f8559f0026485d90
 class BalancingTree:
+    """
+    Self-balancing binary search tree using pivot values.
+
+    See:
+    https://qiita.com/Kiri8128/items/6256f8559f0026485d90
+    """
+
     def __init__(self, n):
         self.N = n
-        self.root = self.node(1<<n, 1<<n)
+        self.root = self.node(1 << n, 1 << n)
 
-    def append(self, v):# v を追加（その時点で v はない前提）
+    def append(self, v): # Note: v does not exist in the tree.
         v += 1
         nd = self.root
+
         while True:
             if v == nd.value:
-                # v がすでに存在する場合に何か処理が必要ならここに書く
+                # v is already in the tree.
+                # TODO: Write code here, if do something.
                 return 0
             else:
                 mi, ma = min(v, nd.value), max(v, nd.value)
+
                 if mi < nd.pivot:
                     nd.value = ma
+
                     if nd.left:
                         nd = nd.left
                         v = mi
                     else:
                         p = nd.pivot
-                        nd.left = self.node(mi, p - (p&-p)//2)
+                        nd.left = self.node(mi, p - (p & -p) // 2)
                         break
                 else:
                     nd.value = mi
+
                     if nd.right:
                         nd = nd.right
                         v = ma
                     else:
                         p = nd.pivot
-                        nd.right = self.node(ma, p + (p&-p)//2)
+                        nd.right = self.node(ma, p + (p & -p) // 2)
                         break
 
     def leftmost(self, nd):
-        if nd.left: return self.leftmost(nd.left)
+        if nd.left: 
+            return self.leftmost(nd.left)
         return nd
 
     def rightmost(self, nd):
-        if nd.right: return self.rightmost(nd.right)
+        if nd.right: 
+            return self.rightmost(nd.right)
         return nd
 
-    def find_l(self, v): # vより真に小さいやつの中での最大値（なければ-1）
+    def find_l(self, v):
+        """The maximum value among the values truly less than v (If not, -1).
+        """
+
         v += 1
         nd = self.root
         prev = 0
-        if nd.value < v: prev = nd.value
+
+        if nd.value < v: 
+            prev = nd.value
+
         while True:
             if v <= nd.value:
                 if nd.left:
@@ -57,19 +74,28 @@ class BalancingTree:
                     return prev - 1
             else:
                 prev = nd.value
+
                 if nd.right:
                     nd = nd.right
                 else:
                     return prev - 1
 
-    def find_r(self, v): # vより真に大きいやつの中での最小値（なければroot）
+    def find_r(self, v):
+        """The smallest value among the values truly greater than v 
+        (if not, root).
+        """
+
         v += 1
         nd = self.root
         prev = 0
-        if nd.value > v: prev = nd.value
+
+        if nd.value > v: 
+            prev = nd.value
+
         while True:
             if v < nd.value:
                 prev = nd.value
+
                 if nd.left:
                     nd = nd.left
                 else:
@@ -82,18 +108,23 @@ class BalancingTree:
 
     @property
     def max(self):
-        return self.find_l((1<<self.N)-1)
+        return self.find_l((1 << self.N) - 1)
 
     @property
     def min(self):
         return self.find_r(-1)
 
-    def delete(self, v, nd = None, prev = None): # 値がvのノードがあれば削除（なければ何もしない）
+    def delete(self, v, nd = None, prev = None):
         v += 1
-        if not nd: nd = self.root
-        if not prev: prev = nd
+
+        if not nd: 
+            nd = self.root
+        if not prev: 
+            prev = nd
+
         while v != nd.value:
             prev = nd
+
             if v <= nd.value:
                 if nd.left:
                     nd = nd.left
@@ -106,6 +137,7 @@ class BalancingTree:
                 else:
                     #####
                     return
+
         if (not nd.left) and (not nd.right):
             if not prev.left:
                 prev.right = None
@@ -142,24 +174,30 @@ class BalancingTree:
 
         def debug_node(nd):
             re = []
+
             if nd.left:
                 re += debug_node(nd.left)
-            if nd.value: re.append(debug_info(nd))
+            if nd.value: 
+                re.append(debug_info(nd))
             if nd.right:
                 re += debug_node(nd.right)
             return re
+
         print("Debug - root =", self.root.value - 1, debug_node(self.root)[:50])
 
     def debug_list(self):
         def debug_node(nd):
             re = []
+
             if nd.left:
                 re += debug_node(nd.left)
-            if nd.value: re.append(nd.value - 1)
+            if nd.value: 
+                re.append(nd.value - 1)
             if nd.right:
                 re += debug_node(nd.right)
             return re
         return debug_node(self.root)[:-1]
+
 
 def main():
     import sys
