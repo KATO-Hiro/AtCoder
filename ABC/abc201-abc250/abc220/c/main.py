345fac0c3870986aff326e0b047af401ce91c387
Kato Hiroki <k.hiro1818@gmail.com>
2021-10-02 07:46:32 +0900

    Solve ABC220 task C

---
+++ ABC/abc201-abc250/abc220/c/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+    x = int(input())
+    ans = float('inf')
+    summed = 0
+
+    for index, ai in enumerate(a, 1):
+        summed += ai
+
+        if summed > x:
+            ans = min(ans, index)
+            print(ans)
+            exit()
+
+    summed_a = sum(a)
+    p, q = divmod(x, summed_a)
+    candidate = p * n
+    summed = 0
+
+    for index, ai in enumerate(a, 1):
+        summed += ai
+
+        if summed > q:
+            ans = min(ans, candidate + index)
+            print(ans)
+            exit()
+
+
+if __name__ == "__main__":
+    main()
