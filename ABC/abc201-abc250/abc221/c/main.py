875e65a844bffea8b1ebaa21f73621bf39164dbe
Kato Hiroki <k.hiro1818@gmail.com>
2021-10-07 07:06:01 +0900

    Refactoring

--- ABC/abc201-abc250/abc221/c/main.py
+++ ABC/abc201-abc250/abc221/c/main.py
@@ -14,15 +14,6 @@ def main():
             former = pattern[:i]
             latter = pattern[i:]
 
-            # Verify leading zeros
-            former2 = tuple(str(int(''.join(former))))
-            latter2 = tuple(str(int(''.join(latter))))
-
-            if len(former) != len(former2):
-                continue
-            if len(latter) != len(latter2):
-                continue
-
             ans = max(ans, int(''.join(former)) * int(''.join(latter)))
 
     print(ans)
