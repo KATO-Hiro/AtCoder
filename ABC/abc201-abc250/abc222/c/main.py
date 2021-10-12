3a390db72a6d2050a7d4d64ff2d5ae32be210a50
Kato Hiroki <k.hiro1818@gmail.com>
2021-10-13 08:21:04 +0900

    Refactoring

--- ABC/abc201-abc250/abc222/c/main.py
+++ ABC/abc201-abc250/abc222/c/main.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 
 
+def judge(first_hand, second_hand):
+    # -1: draw
+    # 0: first_hand win
+    # 1: second_hand win
+
+    if first_hand == second_hand:
+        return -1
+    elif first_hand == 'G' and second_hand == 'C':
+        return 0
+    elif first_hand == 'C' and second_hand == 'P':
+        return 0
+    elif first_hand == 'P' and second_hand == 'G':
+        return 0
+    
+    return 1
+
+
 def main():
     n, m = map(int, input().split())
     a = [list(input()) for _ in range(2 * n)]
@@ -9,26 +26,19 @@ def main():
     for j in range(m):
         order = [(count, index) for index, count in enumerate(win_count)]
         sorted_order = sorted(order, key=lambda x: (x[0], -x[1]) ,reverse=True)
-        aj = [a[i][j] for i in range(2 * n)]
 
         for i in range(n):
             first_id = sorted_order[2 * i][1]
+            first_hand = a[first_id][j]
+
             second_id = sorted_order[2 * i + 1][1]
+            second_hand = a[second_id][j]
 
-            first_hand = aj[first_id]
-            second_hand = aj[second_id]
+            result = judge(first_hand, second_hand)
 
-            if first_hand == 'G' and second_hand == 'C':
+            if result == 0:
                 win_count[first_id] += 1
-            elif first_hand == 'C' and second_hand == 'G':
-                win_count[second_id] += 1
-            elif first_hand == 'C' and second_hand == 'P':
-                win_count[first_id] += 1
-            elif first_hand == 'P' and second_hand == 'C':
-                win_count[second_id] += 1
-            elif first_hand == 'P' and second_hand == 'G':
-                win_count[first_id] += 1
-            elif first_hand == 'G' and second_hand == 'P':
+            elif result == 1:
                 win_count[second_id] += 1
     
     order = [(count, index) for index, count in enumerate(win_count)]
