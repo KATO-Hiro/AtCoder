691fc042714efe75da63722814c2395d9686cd02
Kato Hiroki <k.hiro1818@gmail.com>
2021-11-10 14:22:23 +0900

    Solve ABC226 task D

---
+++ ABC/abc201-abc250/abc226/d/main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    from math import gcd
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    x = [tuple(map(int, input().split())) for _ in range(n)]
+    ans = set()
+
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+
+            xi = x[i]
+            xj = x[j]
+
+            dx = xj[0] - xi[0]
+            dy = xj[1] - xi[1]
+
+            g = gcd(dx, dy)
+            ans.add((dx // g, dy // g))
+    
+    print(len(ans))
+
+
+if __name__ == "__main__":
+    main()
