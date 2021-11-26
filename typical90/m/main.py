efd4e4ea4901a3af73c31429d9affdfc30507f0c
Kato Hiroki <k.hiro1818@gmail.com>
2021-11-27 07:54:36 +0900

    Solve typical90 task M

---
+++ typical90/m/main.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+
+def dijkstra(vertex_count: int, source: int, edges):
+    """Uses Dijkstra's algorithm to find the shortest path in a graph.
+    Args:
+        vertex_count: The number of vertices.
+        source      : Vertex number (0-indexed).
+        edges       : List of (cost, edge) (0-indexed).
+    Returns:
+        costs  : List of the shortest distance.
+        parents: List of parent vertices.
+    Landau notation: O(|Edges|log|Vertices|).
+    See:
+    https://atcoder.jp/contests/abc191/submissions/19964078
+    https://atcoder.jp/contests/abc191/submissions/19966232
+    """
+
+    from heapq import heappop, heappush
+
+    hq = [(0, source)]  # weight, vertex number (0-indexed)
+    costs = [float("inf") for _ in range(vertex_count)]
+    costs[source] = 0
+    visited = [False for _ in range(vertex_count)]
+    pending = -1
+    parents = [pending for _ in range(vertex_count)]
+
+    while hq:
+        cost, vertex = heappop(hq)
+
+        if cost > costs[vertex]:
+            continue
+
+        if visited[vertex]:
+            continue
+
+        visited[vertex] = True
+
+        for weight, edge in edges[vertex]:
+            new_cost = cost + weight
+
+            if new_cost < costs[edge]:
+                costs[edge] = new_cost
+                parents[edge] = vertex
+                heappush(hq, (new_cost, edge))
+
+    return costs, parents
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n)]
+    
+    for _ in range(m):
+        ai, bi, ci = map(int, input().split())
+        ai -= 1
+        bi -= 1
+    
+        graph[ai].append((ci,bi))
+        graph[bi].append((ci,ai))
+    
+    costs1, _ = dijkstra(n, 0, graph)
+    costs2, _ = dijkstra(n, n - 1, graph)
+
+    for c1, c2 in zip(costs1, costs2):
+        print(c1 + c2)
+
+
+if __name__ == "__main__":
+    main()
