e725013d2b0d0e0bf511d7082835b36faf5a9069
Kato Hiroki <k.hiro1818@gmail.com>
2021-12-14 06:33:25 +0900

    Solve joi2022 yo1c task A

---
+++ Others/joi/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    a = int(input())
+    b = int(input())
+    print(b - a)
+
+
+if __name__ == "__main__":
+    main()
