cceaa0727a05d10b18e1668eb5a260e648807e11
Kato Hiroki <k.hiro1818@gmail.com>
2021-12-26 14:21:54 +0900

    Refactoring

--- typical90/cc/main.py
+++ typical90/cc/main.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 
 
+class TwoDimList:
+    """
+    See:
+    https://atcoder.jp/contests/typical90/submissions/23891910
+    """
+
+    def __init__(self, h, w):
+        self.h = h
+        self.w = w
+        self.arr = [0] * (h * w)
+
+    def read(self, hi, wi):
+        return self.arr[self.w * hi + wi]
+
+    def insert(self, hi, wi, value):
+        self.arr[hi * self.w + wi] = value
+
+    def plus(self, hi, wi, value):
+        self.arr[hi * self.w + wi] += value
+
+
 def main():
     import sys
 
@@ -8,28 +29,34 @@ def main():
 
     n, k = map(int, input().split())
     size = 5001
-    ab = [[0 for _ in range(size)] for _ in range(size)]
+    td = TwoDimList(size, size)
 
     for i in range(n):
         ai, bi = map(int, input().split())
-        ab[ai][bi] += 1
+        td.plus(ai, bi, 1)
     
     for j in range(size):
         for i in range(1, size):
-            ab[j][i] += ab[j][i - 1]
+            td.plus(j, i, td.read(j, i - 1))
 
     for i in range(size):
         for j in range(1, size):
-            ab[j][i] += ab[j - 1][i]
+            td.plus(j, i, td.read(j - 1, i))
     
     ans = 1
 
     for a in range(1, size):
-        max_a = min(5000, a + k)
+        if a + k >= size:
+            continue
+
+        max_a = a + k
 
         for b in range(1, size):
-            max_b = min(5000, b + k)
-            candidate = ab[max_a][max_b] - ab[max_a][b - 1] - ab[a - 1][max_b] + ab[a - 1][b - 1]
+            if b + k >= size:
+                continue
+
+            max_b = b + k
+            candidate = td.read(max_a, max_b) - td.read(max_a, b - 1) - td.read(a - 1, max_b) + td.read(a - 1, b - 1)
             ans = max(ans, candidate)
 
     print(ans)
