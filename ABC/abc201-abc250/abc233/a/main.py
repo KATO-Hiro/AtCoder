c6e399d26ae0f5ba0072f53aa667510dadf623d4
Kato Hiroki <k.hiro1818@gmail.com>
2021-12-27 06:34:16 +0900

    Refactoring

--- ABC/abc201-abc250/abc233/a/main.py
+++ ABC/abc201-abc250/abc233/a/main.py
@@ -2,19 +2,18 @@
 
 
 def main():
+    from math import ceil
     import sys
 
     input = sys.stdin.readline
 
     x, y = map(int, input().split())
-    total = x
-    count = 0
 
-    while total < y:
-        total += 10
-        count += 1
-    
-    print(count)
+    if y - x <= 0:
+        print(0)
+    else:
+        y -= x
+        print(ceil(y / 10))
 
 
 if __name__ == "__main__":
