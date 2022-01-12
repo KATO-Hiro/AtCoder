ce61ee090af456a606021c6346526c599a703bb3
Kato Hiroki <k.hiro1818@gmail.com>
2022-01-12 16:10:43 +0900

    Refactoring

--- ABC/abc201-abc250/abc234/d/main.py
+++ ABC/abc201-abc250/abc234/d/main.py
@@ -1,69 +1,26 @@
 # -*- coding: utf-8 -*-
 
 
-from heapq import heapify, heappop, heappush
-from typing import List, Optional
+from typing import List
 
 
-class DeletableHeapq:
-    """Alternatives to ordered set (set) in C++.
-    Landau notation: O(log(n))
+def get_kth_greatest_value(array: List[int], k: int) -> List[int]:
+    """
     See:
-    https://qiita.com/physharp/items/f9229ab879cac9a944d7
-    https://prd-xxx.hateblo.jp/entry/2019/06/24/235844
+    https://atcoder.jp/contests/abc234/submissions/28385666
     """
 
-    def __init__(self, descending_order=False) -> None:
-        self.q: List[int] = []  # Body
-        self.p: List[int] = []  # For deleting
-        self.descending_order = descending_order
-        self.sign = -1 if descending_order else 1
+    from heapq import heapify, heappushpop
 
-    def build(self, a: List[int]) -> None:
-        """Build a priority-queue q from an array."""
+    q = array[:k]
+    heapify(q)
+    ans = [q[0]]
 
-        if self.descending_order:
-            a = [-ai for ai in a]
+    for vi in array[k:]:
+        heappushpop(q, vi)
+        ans.append(q[0])
 
-        self.q = a
-        heapify(self.q)
-
-    def push(self, number: int) -> None:
-        """Add a number to the priority-queue."""
-        heappush(self.q, number * self.sign)
-
-    def erase(self, number: int) -> None:
-        """Pseudo-erase a number to the priority-queue."""
-        heappush(self.p, number * self.sign)
-        self.clean()
-
-    def clean(self) -> None:
-        """Remove top elements from q, p."""
-
-        while self.p and self.q[0] == self.p[0]:
-            heappop(self.q)
-            heappop(self.p)
-
-    def pop(self, exc=None) -> Optional[int]:
-        """Pop a top value from the priority-queue."""
-        self.clean()
-
-        if self.q:
-            return heappop(self.q) * self.sign
-        return exc
-
-    def top(self, exc=None) -> Optional[int]:
-        """Get a top value from the priority-queue.
-        Landau notation: O(1)
-        Note:
-        descending_order=False: min value.
-        descending_order=True : max value.
-        """
-        self.clean()
-
-        if self.q:
-            return self.q[0] * self.sign
-        return exc
+    return ans
 
 
 def main():
@@ -73,17 +30,10 @@ def main():
 
     n, k = map(int, input().split())
     p = list(map(int, input().split()))
+    results = get_kth_greatest_value(p, k)
 
-    # Ascending-order: Get min value in numbers.
-    hq = DeletableHeapq()
-    hq.build(p[:k])
-
-    for i in range(k, n + 1):
-        print(hq.top())
-
-        if i < n:
-            hq.push(p[i])
-            hq.pop()
+    for result in results:
+        print(result)
     
 
 if __name__ == "__main__":
