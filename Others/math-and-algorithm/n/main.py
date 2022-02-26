82dbb0db94bf13eb29bd0c08beb3ea406acd77ae
Kato Hiroki <k.hiro1818@gmail.com>
2022-02-26 21:18:56 +0900

    Solve math and algorithm task N

---
+++ Others/math-and-algorithm/n/main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+
+def run_prime_factorization(max_number: int) -> dict:
+    '''Run prime factorization.
+    Args:
+        max_number: Int of number (greater than 1).
+    Returns:
+        A dictionary's items ((base, exponent) pairs).
+    Landau notation: O(log n)
+    '''
+
+    from math import sqrt
+
+    ans = dict()
+    remain = max_number
+
+    for base in range(2, int(sqrt(max_number)) + 1):
+        if remain % base == 0:
+            exponent_count = 0
+
+            while remain % base == 0:
+                exponent_count += 1
+                remain //= base
+
+            ans[base] = exponent_count
+
+    if remain != 1:
+        ans[remain] = 1
+
+    return ans
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+    
+    n = int(input())
+    f = run_prime_factorization(n)
+    ans = list()
+
+    for key, value in f.items():
+        for _ in range(value):
+            ans.append(key)
+    
+    print(*sorted(ans))
+
+
+
+if __name__ == "__main__":
+    main()
+
