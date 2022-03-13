7ab29b34bf6da253ecb05fdc26407e9e13f2aedf
Kato Hiroki <k.hiro1818@gmail.com>
2022-03-14 06:24:20 +0900

    Solve math and algorithm task P

---
+++ Others/math-and-algorithm/p/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    from math import gcd
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+    ans = a[0]
+
+    for ai in a[1:]:
+        ans = gcd(ans, ai)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
