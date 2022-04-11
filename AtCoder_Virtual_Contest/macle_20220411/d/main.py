3620e4ce1981ef822eca1d4702806b045d221ee7
Kato Hiroki <k.hiro1818@gmail.com>
2022-04-11 22:02:17 +0900

    Solve ABC167E

---
+++ AtCoder_Virtual_Contest/macle_20220411/d/main.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+
+class Combination:
+    ''' Count the total number of combinations.
+        nCr % mod.
+        nHr % mod = (n + r - 1)Cr % mod.
+        Args:
+            max_value: Max size of list. The default is 500,050
+            mod      : Modulo. The default is 10 ** 9 + 7.
+        Landau notation: O(n)
+        See:
+        http://drken1215.hatenablog.com/entry/2018/06/08/210000
+    '''
+
+    def __init__(self, max_value=500050, mod=10 ** 9 + 7):
+        self.max_value = max_value
+        self.mod = mod
+        self.fac = [0 for _ in range(self.max_value)]
+        self.finv = [0 for _ in range(self.max_value)]
+        self.inv = [0 for _ in range(self.max_value)]
+
+        self.fac[0] = 1
+        self.fac[1] = 1
+        self.finv[0] = 1
+        self.finv[1] = 1
+        self.inv[1] = 1
+
+        for i in range(2, self.max_value):
+            self.fac[i] = self.fac[i - 1] * i % self.mod
+            self.inv[i] = self.mod - self.inv[self.mod % i] * (self.mod // i) % self.mod
+            self.finv[i] = self.finv[i - 1] * self.inv[i] % self.mod
+
+    def count_nCr(self, n, r):
+        '''Count the total number of combinations.
+            nCr % mod.
+            nHr % mod = (n + r - 1)Cr % mod.
+        Args:
+            n   : Elements. Int of number (greater than 1).
+            r   : The number of r-th combinations. Int of number
+                  (greater than 0).
+        Returns:
+            The total number of combinations.
+        Landau notation: O(1)
+        '''
+
+        if n < r:
+            return 0
+        if n < 0 or r < 0:
+            return 0
+
+        return self.fac[n] * (self.finv[r] * self.finv[n - r] % self.mod) % self.mod
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, m, k = map(int, input().split())
+    mod = 998244353
+    c = Combination(2 * 10 ** 5 + 10, mod)
+    ans = 0
+
+    for i in range(k + 1):
+        nCr = c.count_nCr(n - 1, i)
+        groups = m * pow(m - 1, n - 1 - i, mod)
+        ans += nCr * groups 
+        ans %= mod
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
