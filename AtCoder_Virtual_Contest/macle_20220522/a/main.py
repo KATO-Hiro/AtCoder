a69987e2b15e132a46c723f01b6bd0a7c973a9ae
Kato Hiroki <k.hiro1818@gmail.com>
2022-05-22 22:03:11 +0900

    Upsolve ABC166A

---
+++ AtCoder_Virtual_Contest/macle_20220522/a/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    s = input().rstrip()
+
+    if s == "ABC":
+        print("ARC")
+    else:
+        print("ABC")
+
+
+if __name__ == "__main__":
+    main()
