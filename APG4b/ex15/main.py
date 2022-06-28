4dc9e0afbb0a6e1f6b9ea431089fd2c2f8fa64d1
Kato Hiroki <k.hiro1818@gmail.com>
2022-06-28 21:55:38 +0900

    Solve APG4b EX15

---
+++ APG4b/ex15/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    c = list(map(int, input().split()))
+
+    print(sum(a) * sum(b) * sum(c))
+
+
+if __name__ == "__main__":
+    main()
