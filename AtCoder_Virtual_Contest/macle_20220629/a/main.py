1e078d58e7415eda825a0be1c0b7c98bfae32867
Kato Hiroki <k.hiro1818@gmail.com>
2022-06-29 21:56:35 +0900

    Upsolve ABC162B

---
+++ AtCoder_Virtual_Contest/macle_20220629/a/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    ans = 0
+
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            continue
+        elif i % 5 == 0:
+            continue
+        elif i % 3 == 0:
+            continue
+
+        ans += i
+    
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
