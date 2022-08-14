84e63d13752b8e0b84ea332734213d5ddb2db8e6
Kato Hiroki <k.hiro1818@gmail.com>
2022-08-14 22:41:16 +0900

    Solve APG4b task EX23

---
+++ APG4b/ex23/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    from collections import Counter
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+    c = Counter(a)
+    print(*c.most_common()[0])
+
+
+if __name__ == "__main__":
+    main()
