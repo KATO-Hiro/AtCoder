0e541bb765ea1918f8d5d1058e4dcc7ba4643202
Kato Hiroki <k.hiro1818@gmail.com>
2022-08-30 22:11:31 +0900

    Upsolve ABC180C

---
+++ AtCoder_Virtual_Contest/macle_20220830/a/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    from math import sqrt
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    ans = set()
+
+    for i in range(1, int(sqrt(n)) + 1):
+        if n % i == 0:
+            ans.add(i)
+            ans.add(n // i)
+    
+    print(*sorted(ans), sep="\n")
+
+
+if __name__ == "__main__":
+    main()
