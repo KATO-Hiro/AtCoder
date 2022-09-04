7b3b082802355461ef0306b14a6b78893a1ca8ab
Kato Hiroki <k.hiro1818@gmail.com>
2022-09-04 22:22:10 +0900

    Refactoring

--- ABC/abc251-abc300/abc267/c/main.py
+++ ABC/abc251-abc300/abc267/c/main.py
@@ -2,29 +2,29 @@
 
 
 def main():
-    from itertools import accumulate
     import sys
 
     input = sys.stdin.readline
 
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
-    c = list(accumulate([0] + a))
-    ans = 0
+    summed, total = 0, 0
 
     # 最初だけ愚直に
     for i in range(1, m + 1):
-        ans += i * a[i - 1]
-    
-    pre = ans
+        summed += i * a[i - 1]
+        total += a[i - 1]
+
+    ans = summed
     
     # 差分を計算
     for i in range(m, n):
-        candidate = pre
-        candidate -= c[i] - c[i - m]
-        candidate += m * a[i]
-        ans = max(ans, candidate)
-        pre = candidate
+        n_summed = summed - total + a[i] * m 
+        n_total = total - a[i - m]+ a[i] 
+        ans = max(ans, n_summed)
+
+        summed = n_summed
+        total = n_total
 
     print(ans)
 
