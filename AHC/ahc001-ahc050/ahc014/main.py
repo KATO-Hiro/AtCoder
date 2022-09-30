1661d240e3681fa12d0984a812ca27d647237d37
Kato Hiroki <k.hiro1818@gmail.com>
2022-10-01 08:56:49 +0900

    Solve ahc014 task A by simple solution

---
+++ AHC/ahc001-ahc050/ahc014/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+
+    for i in range(m):
+        xi, yi = map(int, input().split())
+
+    print(0)
+
+
+if __name__ == "__main__":
+    main()
