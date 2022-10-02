2fb404137fdc6b1658fa3740715e63414f56f80e
Kato Hiroki <k.hiro1818@gmail.com>
2022-10-02 10:21:06 +0900

    Refactoring

--- ABC/abc251-abc300/abc271/c/main.py
+++ ABC/abc251-abc300/abc271/c/main.py
@@ -1,52 +1,37 @@
 # -*- coding: utf-8 -*-
 
 
+def f(wj, a, n):
+    # 連続する要素の長さを求める
+    count = len(set(range(1, wj + 1)) & a)
+
+    # そのまま読むことができる冊数 + 2冊売って買った冊数
+    if count + (n - count) // 2 >= wj:
+        return True
+    else:
+        return False
+
+
 def main():
-    from collections import deque, Counter
     import sys
 
     input = sys.stdin.readline
 
     n = int(input())
-    a = list(map(int, input().split()))
-    c = Counter(a)
-    b = list()
-    inf = 10 ** 12
+    a = set(map(int, input().split()))  # 集合として扱う
+    ok, ng = -1, 10 ** 6
 
-    for key, value in c.items():
-        if value > 1:
-            for i in range(value - 1):
-                b.append(inf)
+    # 連続して本が読める = 単調性
+    # 答えを決め打ちして二分探索
+    while abs(ok - ng) > 1:
+        wj = (ok + ng) // 2
 
-        b.append(key)
-
-    d = deque(sorted(b))
-    cur = 1
-    ans = 0
-
-    # コーナーケース
-    # 同じaiが2つ以上ある場合やai > nの場合をinfとして扱う
-    while n > 0:
-        now = d[0]
-
-        if cur == now:
-            d.popleft()
-            n -= 1
-            ans += 1
-            cur += 1
+        if f(wj, a, n):
+            ok = wj
         else:
-            if n >= 2:
-                n -= 2
-                d.pop()
-                d.pop()
+            ng = wj
 
-                ans += 1
-                cur += 1
-            else:
-                print(ans)
-                exit()
-
-    print(ans)
+    print(ok)
 
 
 if __name__ == "__main__":
