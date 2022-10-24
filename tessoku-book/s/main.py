537f3f78f2d2ac08bcc8d5845b1e1715df0e5fec
Kato Hiroki <k.hiro1818@gmail.com>
2022-10-25 07:01:09 +0900

    Refactoring

--- tessoku-book/s/main.py
+++ tessoku-book/s/main.py
@@ -7,20 +7,14 @@ def main():
     input = sys.stdin.readline
 
     n, w = map(int, input().split())
-    wv = [tuple(map(int, input().split())) for _ in range(n)]
     dp = [0] * (w + 1)
 
-    for wi, vi in wv:
-        ndp = [0] * (w + 1)
+    for i in range(n):
+        wi, vi = map(int, input().split())
 
-        for j in range(w + 1):
-            ndp[j] = max(ndp[j], dp[j])
+        for j in range(w, wi - 1, -1):
+            dp[j] = max(dp[j], dp[j - wi] + vi)
 
-            if j - wi >= 0:
-                ndp[j] = max(ndp[j], dp[j - wi] + vi)
-
-        dp = ndp
-    
     print(dp[w])
 
 
