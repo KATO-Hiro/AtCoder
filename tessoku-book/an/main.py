e9db96d9069ba891fb9c1f5651fe1d7732545928
Kato Hiroki <k.hiro1818@gmail.com>
2022-10-30 22:41:53 +0900

    Solve tessoku-book task AN

---
+++ tessoku-book/an/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    from collections import Counter
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+    c = Counter(a)
+    ans = 0
+
+    for key, value in c.items():
+        ans += value * (value - 1) * (value - 2) // 6
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
