23b57eaa3ca67b09f860fbb8618d9468294275ed
Kato Hiroki <k.hiro1818@gmail.com>
2022-11-03 14:06:09 +0900

    Upolve ABC275B

--- ABC/abc251-abc300/abc275/b/main.py
+++ ABC/abc251-abc300/abc275/b/main.py
@@ -8,19 +8,8 @@ def main():
 
     a, b, c, d, e, f = map(int, input().split())
     mod = 998244353
-    ans = 1
-
-    ans = a * b
-    ans *= c
-    ans %= mod
-    
-    ans2 = 1
-    ans2 = d * e
-    ans2 %= mod
-    ans2 *= f
-    ans2 %= mod
-
-    print((ans - ans2) % mod)
+    ans = (a * b * c - d * e * f) % mod
+    print(ans)
 
 
 if __name__ == "__main__":
