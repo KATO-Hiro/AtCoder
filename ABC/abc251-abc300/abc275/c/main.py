cf5034b14ee6a9ea76b56aeb441bd42cfa51600f
Kato Hiroki <k.hiro1818@gmail.com>
2022-11-03 14:42:56 +0900

    Upsolve ABC275C

--- ABC/abc251-abc300/abc275/c/main.py
+++ ABC/abc251-abc300/abc275/c/main.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 
 
+# points: [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+def is_square(points):
+    # See:
+    # https://sugim48.hatenadiary.org/entry/20141007/1412679470
+
+    dist = list() 
+
+    for i in range(4):
+        x1, y1 = points[i]
+
+        for j in range(i + 1, 4):
+            x2, y2 = points[j]
+
+            dx = (x2 - x1) ** 2
+            dy = (y2 - y1) ** 2
+
+            dist.append(dx + dy)
+
+    dist = sorted(dist)
+    base = dist[0]
+
+    if base == 0:
+        return False # duplicated
+    
+    if dist.count(base) == 4 and dist.count(base * 2) == 2:
+        return True
+    else:
+        return False
+
+
 def main():
     from itertools import combinations
     import sys
@@ -14,26 +44,15 @@ def main():
     for x in range(9):
         for y in range(9):
             if s[y][x] == "#":
-                xy.append((count, x, y))
+                xy.append((x, y))
                 count += 1
             
     ans = 0
     
     for a, b, c, d in combinations(range(count), 4):
-        dist = set()
-
-        for e, f in combinations([a, b, c, d], 2):
-            _, ex, ey =  xy[e]
-            _, fx, fy =  xy[f]
-            d = (ex - fx) ** 2 + (ey - fy) ** 2
-
-            dist.add(d)
-    
-        if len(dist) == 2:
+        if is_square((xy[a], xy[b], xy[c], xy[d])):
             ans += 1
 
-    # print(s)
-    # print(xy)
     print(ans)
     
 
