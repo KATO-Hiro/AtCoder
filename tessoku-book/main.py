727a7ee5532a653aeea529076f6cccc1cc10b975
Kato Hiroki <k.hiro1818@gmail.com>
2022-11-17 06:24:57 +0900

    Solve tessoku-book task EZ

---
+++ tessoku-book/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = sorted(list(map(int, input().split())), reverse=True)
+    print(sum(a[:2]))
+    
+
+if __name__ == "__main__":
+    main()
