47177941fcb9244774c68e3da7c755794f3689bb
Kato Hiroki <k.hiro1818@gmail.com>
2022-11-28 21:53:45 +0900

    Solve tessoku-book task C

---
+++ tessoku-book/c/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, k = map(int, input().split())
+    p = list(map(int, input().split()))
+    q = list(map(int, input().split()))
+
+    for pi in p:
+        for qi in q:
+            if pi + qi == k:
+                print("Yes")
+                exit()
+    
+    print("No")
+
+
+if __name__ == "__main__":
+    main()
