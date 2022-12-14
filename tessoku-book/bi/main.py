1f6ebbb1a85258a66c46c3e1d2be7ca300beb044
Kato Hiroki <k.hiro1818@gmail.com>
2022-12-14 21:34:06 +0900

    Solve tessoku-book task BI

---
+++ tessoku-book/bi/main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+    
+    for _ in range(m):
+        ai, bi = map(int, input().split())
+    
+        graph[ai].append(bi)
+        graph[bi].append(ai)
+    
+    for i, g in enumerate(graph):
+        if i == 0:
+            continue
+
+        print(i, end="")
+        print(": ", end="")
+        print("{", end="")
+        print(*sorted(g), sep=", ", end="")
+        print("}")
+
+
+if __name__ == "__main__":
+    main()
