f57fd783675bdc01772552c62b9c1e5be8ced981
Kato Hiroki <k.hiro1818@gmail.com>
2022-12-20 06:43:05 +0900

    Solve ABC279A

---
+++ ABC/abc251-abc300/abc279/a/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    s = input().rstrip()
+    ans = 0
+
+    for si in s:
+        if si == "v":
+            ans += 1
+        else:
+            ans += 2
+    
+    print(ans)
+    
+
+if __name__ == "__main__":
+    main()
