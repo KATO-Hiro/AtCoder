254e3c89e09b0a60e4401e96af78a5ea268c11ae
Kato Hiroki <k.hiro1818@gmail.com>
2022-12-24 23:38:13 +0900

    Refactoring

--- ABC/abc251-abc300/abc281/b/main.py
+++ ABC/abc251-abc300/abc281/b/main.py
@@ -1,22 +1,31 @@
 # -*- coding: utf-8 -*-
 
 
-def main():
+def is_met_conditions_using_regular_expressions(strings, pattern):
+    # See:
+    # https://qiita.com/luohao0404/items/7135b2b96f9b0b196bf3
+    # https://atcoder.jp/contests/abc281/submissions/37139022
+
     import re
+
+    r = re.compile(pattern)
+    result = r.fullmatch(strings)
+
+    if result:
+        return "Yes"
+    else:
+        return "No"
+
+
+def main():
     import sys
 
     input = sys.stdin.readline
 
     s = input().rstrip()
-    pattern = "[A-Z][1-9][0-9]{5}[A-Z]"
+    pattern = r"[A-Z][1-9]\d{5}[A-Z]"
 
-    r = re.compile(pattern)
-    result = r.match(s)
-
-    if result:
-        print("Yes")
-    else:
-        print("No")
+    print(is_met_conditions_using_regular_expressions(s, pattern))
     
 
 if __name__ == "__main__":
