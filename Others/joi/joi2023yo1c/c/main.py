c86d4990e02d922263ac27cff4e2fa7e2de27ce1
Kato Hiroki <k.hiro1818@gmail.com>
2023-01-31 23:23:24 +0900

    Solve joi2023yo1C task C

---
+++ Others/joi/joi2023yo1c/c/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    s = input().rstrip()
+    print(s.replace("jj", "JJ").replace("oo", "OO").replace("ii", "II"))
+
+
+if __name__ == "__main__":
+    main()
