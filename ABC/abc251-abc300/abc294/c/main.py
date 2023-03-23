5799e084cf761e059ef49de0d062061d2bddaf07
Kato Hiroki <k.hiro1818@gmail.com>
2023-03-23 23:15:50 +0900

    Refactoring

--- ABC/abc251-abc300/abc294/c/main.py
+++ ABC/abc251-abc300/abc294/c/main.py
@@ -9,23 +9,10 @@ def main():
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    c = list()
-
-    for ai in a:
-        c.append((ai, 1))
-    for bi in b:
-        c.append((bi, 2))
-
-    ans_a, ans_b = list(), list()
+    c = {ci: i for i, ci in enumerate(sorted(a + b), 1)}
     
-    for i, (ci, case) in enumerate(sorted(c), 1):
-        if case == 1:
-            ans_a.append(i)
-        else:
-            ans_b.append(i)
-    
-    print(*ans_a)
-    print(*ans_b)
+    print(*[c[ai] for ai in a])
+    print(*[c[bi] for bi in b])
     
 
 if __name__ == "__main__":
