40baba044b68ad13591c3d4b4ae761c0db7a5385
Kato Hiroki <k.hiro1818@gmail.com>
2023-04-10 10:05:38 +0900

    Refactoring (#2634)

--- ABC/abc251-abc300/abc297/c/main.py
+++ ABC/abc251-abc300/abc297/c/main.py
@@ -7,16 +7,10 @@ def main():
     input = sys.stdin.readline
 
     h, w = map(int, input().split())
-    s = [list(input().rstrip()) for _ in range(h)]
+    s = [input().rstrip() for _ in range(h)]
 
-    for i in range(h):
-        for j in range(w - 1):
-            if s[i][j] == "T" and s[i][j + 1] == "T":
-                s[i][j] = "P"
-                s[i][j + 1] = "C"
-    
     for si in s:
-        print(*si, sep="")
+        print(si.replace("TT", "PC"))
     
 
 if __name__ == "__main__":
