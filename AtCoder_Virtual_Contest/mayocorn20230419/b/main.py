36def47e95e63d35b5ad2e7c2c902d474f74cec9
Kato Hiroki <k.hiro1818@gmail.com>
2023-04-20 09:23:38 +0900

    Refactoring

--- AtCoder_Virtual_Contest/mayocorn20230419/b/main.py
+++ AtCoder_Virtual_Contest/mayocorn20230419/b/main.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
-def main():
+def law_of_cosines(a, b, theta_degree):
     from math import cos, radians, sqrt
+
+    c = a ** 2 + b ** 2 - 2 * a * b * cos(radians(theta_degree))
+    return sqrt(c)
+
+
+def main():
     import sys
 
     input = sys.stdin.readline
@@ -10,13 +16,7 @@ def main():
     a, b, h, m = map(int, input().split())
     x = (h * 60 + m) * 360 / 720
     y = m * 360 / 60
-
-    if x < y:
-        x, y = y, x
-    
-    z = x - y
-    c = a ** 2 + b ** 2 - 2 * a * b * cos(radians(z))
-    print(c ** 0.5)
+    print(law_of_cosines(a, b, abs(x - y)))
 
 
 if __name__ == "__main__":
