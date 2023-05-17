dd0b6c2308516fc130db059b2f301c227916bc12
Kato Hiroki <k.hiro1818@gmail.com>
2023-05-17 23:31:24 +0900

    Upsolve ABC129B

---
+++ AtCoder_Virtual_Contest/mayocorn20230517/a/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    w = list(map(int, input().split()))
+    ans = float("inf")
+
+    for t in range(1, n):
+        s1 = sum(w[:t])
+        s2 = sum(w[t:])
+
+        ans = min(ans, abs(s1 - s2))
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
