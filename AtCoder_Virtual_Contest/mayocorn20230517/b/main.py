74ef39761d86c90eda35459794aaf5b73267c9ce
Kato Hiroki <k.hiro1818@gmail.com>
2023-05-17 23:31:08 +0900

    Upsolve ABC108B

---
+++ AtCoder_Virtual_Contest/mayocorn20230517/b/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    x1, y1, x2, y2 = map(int, input().split())
+
+    dx = x2 - x1
+    dy = y2 - y1
+
+    print(x2 - dy, y2 + dx, x1 - dy, y1 + dx)
+
+
+if __name__ == "__main__":
+    main()
