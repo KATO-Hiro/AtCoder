4efab7ab4c43f9321b2ef75c26c11db6304c2575
Kato Hiroki <k.hiro1818@gmail.com>
2023-05-26 23:11:27 +0900

    Upsolve ABC155B

---
+++ AtCoder_Virtual_Contest/mayocorn20230526/a/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    for ai in a:
+        if ai % 2 == 0:
+            if ai % 3 == 0 or ai % 5 == 0:
+                pass
+            else:
+                print("DENIED")
+                exit()
+
+    print("APPROVED")
+
+
+if __name__ == "__main__":
+    main()
