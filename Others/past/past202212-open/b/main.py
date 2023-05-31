430e1d4cf8dc224830407d14fde8adfa2d23bef4
Kato Hiroki <k.hiro1818@gmail.com>
2023-05-31 22:23:38 +0900

    Refactoring

--- Others/past/past202212-open/b/main.py
+++ Others/past/past202212-open/b/main.py
@@ -7,10 +7,13 @@ def main():
     input = sys.stdin.readline
 
     a, b, c, d = map(int, input().split())
+    # 小数同士の比較による誤差を回避するため通分 + 分母BDが0以外のため、*BD
+    left = a * b * (d**2)
+    right = c * (b**2) * d
 
-    if a / b < c / d:
+    if left < right:
         print("<")
-    elif a / b > c / d:
+    elif left > right:
         print(">")
     else:
         print("=")
