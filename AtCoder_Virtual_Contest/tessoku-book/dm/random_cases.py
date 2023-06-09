fecd2412aa4fd44f0a9fbdd9f76a64bab310fc03
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-09 22:01:38 +0900

    Run random cases

---
+++ AtCoder_Virtual_Contest/tessoku-book/dm/random_cases.py
@@ -0,0 +1,15 @@
+# ランダムケースの生成
+# random_cases.py
+
+from random import randint
+
+# 制約条件を乱数で生成
+# 一部の制約条件は、愚直な解法で間に合うように調整する
+n = randint(1, 10**3)
+a = [randint(1, 100) for _ in range(n)]
+
+# 制約条件に関する内容を出力
+print(n)
+print(*a)  # リストの場合は、展開しておく必要がある
+
+# 順列の並べ替え（numpy）や木のテストケース作成（networkx）などもできる
