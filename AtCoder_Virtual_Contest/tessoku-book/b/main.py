24a1ba2bf6419eaca68910bf55998374a2502236
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-14 09:01:34 +0900

    Upsolve tessoku B

--- AtCoder_Virtual_Contest/tessoku-book/b/main.py
+++ AtCoder_Virtual_Contest/tessoku-book/b/main.py
@@ -6,6 +6,14 @@ def main():
 
     input = sys.stdin.readline
 
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    if x in a:
+        print("Yes")
+    else:
+        print("No")
+
 
 if __name__ == "__main__":
     main()
