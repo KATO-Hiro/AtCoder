ed1d1a9bec60196ca1b011079e58163f2b005c5f
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-16 09:07:40 +0900

    Upolve tessoku DL

---
+++ AtCoder_Virtual_Contest/tessoku-book/dl/main.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+
+from heapq import heapify, heappop, heappush
+from typing import List, Optional
+
+
+class DeletableHeapq:
+    """Alternatives to ordered set (set) in C++.
+
+    Landau notation: O(log(n))
+
+    See:
+    https://qiita.com/physharp/items/f9229ab879cac9a944d7
+    https://prd-xxx.hateblo.jp/entry/2019/06/24/235844
+    """
+
+    def __init__(self, descending_order=False) -> None:
+        self.q: List[int] = []  # Body
+        self.p: List[int] = []  # For deleting
+        self.descending_order = descending_order
+        self.sign = -1 if descending_order else 1
+
+    def build(self, a: List[int]) -> None:
+        """Build a priority-queue q from an array."""
+
+        if self.descending_order:
+            a = [-ai for ai in a]
+
+        self.q = a
+        heapify(self.q)
+
+    def push(self, number: int) -> None:
+        """Add a number to the priority-queue."""
+        heappush(self.q, number * self.sign)
+
+    def erase(self, number: int) -> None:
+        """Pseudo-erase a number to the priority-queue."""
+        heappush(self.p, number * self.sign)
+        self.clean()
+
+    def clean(self) -> None:
+        """Remove top elements from q, p."""
+
+        while self.p and self.q[0] == self.p[0]:
+            heappop(self.q)
+            heappop(self.p)
+
+    def pop(self, exc=None) -> Optional[int]:
+        """Pop a top value from the priority-queue."""
+        self.clean()
+
+        if self.q:
+            return heappop(self.q) * self.sign
+        return exc
+
+    def top(self, exc=None) -> Optional[int]:
+        """Get a top value from the priority-queue.
+
+        Landau notation: O(1)
+
+        Note:
+        descending_order=False: min value.
+        descending_order=True : max value.
+        """
+        self.clean()
+
+        if self.q:
+            return self.q[0] * self.sign
+        return exc
+
+
+def main():
+    import sys
+    from collections import defaultdict
+
+    input = sys.stdin.readline
+
+    n, d = map(int, input().split())
+    days = defaultdict(list)
+    hq = DeletableHeapq(descending_order=True)
+    size = 0
+
+    for _ in range(n):
+        xi, yi = map(int, input().split())
+        days[xi].append(yi)
+
+    ans = 0
+
+    for i in range(1, d + 1):
+        if len(days) > 0:
+            for value in days[i]:
+                hq.push(value)
+                size += 1
+
+        if size > 0:
+            max_value = hq.top()
+            ans += hq.pop(max_value)
+            size -= 1
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
