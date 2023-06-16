25a046d2e088ba4dc5b2efa39a8d2ae1fcb27233
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-16 15:08:36 +0900

    Refactoring

--- AtCoder_Virtual_Contest/tessoku-book/dn/main.py
+++ AtCoder_Virtual_Contest/tessoku-book/dn/main.py
@@ -10,25 +10,18 @@ def main():
     ans = list()
 
     while x > 1 or y > 1:
+        ans.append((x, y))
+
         if x >= y:
-            ans.append(0)
             x -= y
         else:
-            ans.append(1)
             y -= x
-
-    x, y = 1, 1
 
     # 復元
     print(len(ans))
 
-    for flag in ans[::-1]:
-        if flag:
-            y += x
-        else:
-            x += y
-
-        print(x, y)
+    for xi, yi in ans[::-1]:
+        print(xi, yi)
 
 
 if __name__ == "__main__":
