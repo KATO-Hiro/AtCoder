62f21afcd4ff1c57acde204dbf6f88ca57f795c9
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-16 15:04:53 +0900

    Refactoring

--- AtCoder_Virtual_Contest/tessoku-book/dn/main.py
+++ AtCoder_Virtual_Contest/tessoku-book/dn/main.py
@@ -4,50 +4,31 @@
 def main():
     import sys
 
-    sys.setrecursionlimit(10**6 + 10)
-
     input = sys.stdin.readline
 
     x, y = map(int, input().split())
     ans = list()
 
-    def gcd(x, y):
-        if x == 0 or y == 0:
-            return None, None
-
-        if x == 1 and y == 1:
-            return None, None
-
-        ans.append((x, y))
-
+    while x > 1 or y > 1:
         if x >= y:
-            p, q = divmod(x, y)
-
-            for i in range(p - 1, 0, -1):
-                ans.append((i * y + q, y))
-
-            gcd(x % y, y)
+            ans.append(0)
+            x -= y
         else:
-            p, q = divmod(y, x)
+            ans.append(1)
+            y -= x
 
-            for i in range(p - 1, 0, -1):
-                ans.append((x, i * x + q))
+    x, y = 1, 1
 
-            gcd(x, y % x)
+    # 復元
+    print(len(ans))
 
-    gcd(x, y)
+    for flag in ans[::-1]:
+        if flag:
+            y += x
+        else:
+            x += y
 
-    size = len(ans)
-
-    if (1, 1) in ans:
-        size -= 1
-
-    print(size)
-
-    if size > 0:
-        for xi, yi in ans[::-1]:
-            if xi > 1 or yi > 1:
-                print(xi, yi)
+        print(x, y)
 
 
 if __name__ == "__main__":
