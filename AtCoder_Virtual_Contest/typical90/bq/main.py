c374fd3cad1cf30f0c9dec76d9875429a3ddb686
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-23 14:56:30 +0900

    Upsolve tessoku BQ

---
+++ AtCoder_Virtual_Contest/typical90/bq/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, k = map(int, input().split())
+    mod = 10**9 + 7
+
+    ans = k
+
+    if n >= 2:
+        ans *= (k - 1) % mod
+
+    if n >= 3:
+        ans *= pow(max(0, k - 2), n - 2, mod) % mod
+
+    print(ans % mod)
+
+
+if __name__ == "__main__":
+    main()
