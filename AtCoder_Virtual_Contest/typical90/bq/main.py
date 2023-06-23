8c60a4ca22de3b6d66f665cf7ae4fd5293ec159b
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-23 14:57:44 +0900

    Refactoring

--- AtCoder_Virtual_Contest/typical90/bq/main.py
+++ AtCoder_Virtual_Contest/typical90/bq/main.py
@@ -8,14 +8,10 @@ def main():
 
     n, k = map(int, input().split())
     mod = 10**9 + 7
-
     ans = k
 
     if n >= 2:
-        ans *= (k - 1) % mod
-
-    if n >= 3:
-        ans *= pow(max(0, k - 2), n - 2, mod) % mod
+        ans *= (k - 1) * pow(max(0, k - 2), n - 2, mod) % mod
 
     print(ans % mod)
 
