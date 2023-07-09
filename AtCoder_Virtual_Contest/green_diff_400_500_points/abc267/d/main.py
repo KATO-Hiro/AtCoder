4df54a201d337b04f5eace7eea5ed9146f11f584
Kato Hiroki <k.hiro1818@gmail.com>
2023-07-10 08:30:56 +0900

    Upsolve ABC267D

---
+++ AtCoder_Virtual_Contest/green_diff_400_500_points/abc267/d/main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    inf = 10**12
+    dp = [-inf] * (m + 1)
+    dp[0] = 0
+
+    for i in range(n):
+        ai = a[i]
+        ndp = [-inf] * (m + 1)
+
+        for j in range(m + 1):
+            if dp[j] == -inf:
+                continue
+
+            ndp[j] = max(ndp[j], dp[j])
+
+            if j + 1 <= m:
+                ndp[j + 1] = max(ndp[j + 1], dp[j] + (j + 1) * ai)
+
+        dp = ndp
+
+    print(dp[m])
+
+
+if __name__ == "__main__":
+    main()
