50aaeb466a45c8f8672f3e51a22dec1b508fe44b
Kato Hiroki <k.hiro1818@gmail.com>
2023-07-09 11:14:17 +0900

    Refactoring

--- ABC/abc301-abc350/abc309/a/main.py
+++ ABC/abc301-abc350/abc309/a/main.py
@@ -8,17 +8,7 @@ def main():
 
     a, b = map(int, input().split())
 
-    if a == 1 and b == 2:
-        print("Yes")
-    elif a == 2 and b == 3:
-        print("Yes")
-    elif a == 4 and b == 5:
-        print("Yes")
-    elif a == 5 and b == 6:
-        print("Yes")
-    elif a == 7 and b == 8:
-        print("Yes")
-    elif a == 8 and b == 9:
+    if a % 3 != 0 and (a + 1 == b):
         print("Yes")
     else:
         print("No")
