e523595c069b32035bc93c853747ad3ab73ae47f
Kato Hiroki <k.hiro1818@gmail.com>
2023-07-10 18:45:33 +0900

    Upsolve ABC129D

---
+++ AtCoder_Virtual_Contest/green_diff_400_500_points/abc129/d/main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+
+# See:
+# https://atcoder.jp/contests/hhkb2020/submissions/17301581
+def count_cells(grids, h: int, w: int):
+    dot = "."
+    up = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+    down = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+    left = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+    right = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+
+    # Comment out the following lines if needs.
+    # dot_count = 0
+
+    # for i in range(h):
+    #     for j in range(w):
+    #         if grids[i][j] == dot:
+    #             dot_count += 1
+
+    for i in range(h - 2, -1, -1):
+        for j in range(w):
+            if grids[i][j] == dot:
+                up[i][j] = up[i + 1][j] + 1
+
+    for i in range(1, h):
+        for j in range(w):
+            if grids[i][j] == dot:
+                down[i][j] = down[i - 1][j] + 1
+
+    for i in range(h):
+        for j in range(w - 2, -1, -1):
+            if grids[i][j] == dot:
+                left[i][j] = left[i][j + 1] + 1
+
+    for i in range(h):
+        for j in range(1, w):
+            if grids[i][j] == dot:
+                right[i][j] = right[i][j - 1] + 1
+
+    ans = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grids[i][j] == dot:
+                count = (up[i][j] + down[i][j] - 1) + (left[i][j] + right[i][j] - 1) - 1
+
+                # Update ans using count.
+                ans = max(ans, count)
+
+    return ans
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    h, w = map(int, input().split())
+    s = [list(input().rstrip()) for _ in range(h)]
+    count = count_cells(s, h, w)
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
