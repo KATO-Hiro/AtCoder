3d0557e02ed39708c584f48310a7048da4e25074
Kato Hiroki <k.hiro1818@gmail.com>
2023-07-12 09:14:23 +0900

    Upsolve ABC125C

--- AtCoder_Virtual_Contest/green_diff_400_500_points/abc125/c/main.py
+++ AtCoder_Virtual_Contest/green_diff_400_500_points/abc125/c/main.py
@@ -1,5 +1,147 @@
 # -*- coding: utf-8 -*-
 
+import typing
+
+
+# See:
+# ac-library-python
+# https://github.com/not522/ac-library-python/blob/master/atcoder/segtree.py
+def _ceil_pow2(n: int) -> int:
+    x = 0
+
+    while (1 << x) < n:
+        x += 1
+
+    return x
+
+
+class SegTree:
+    def __init__(
+        self,
+        op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+        e: typing.Any,
+        v: typing.Union[int, typing.List[typing.Any]],
+    ) -> None:
+        self._op = op
+        self._e = e
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = _ceil_pow2(self._n)
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        self._d[p] = x
+
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        return self._d[p + self._size]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+
+        sml = self._e
+        smr = self._e
+        left += self._size
+        right += self._size
+
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def max_right(self, left: int, f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert f(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        sm = self._e
+        first = True
+
+        while first or (left & -left) != left:
+            first = False
+
+            while left % 2 == 0:
+                left >>= 1
+
+            if not f(self._op(sm, self._d[left])):
+                while left < self._size:
+                    left *= 2
+
+                    if f(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int, f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= right <= self._n
+        assert f(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        sm = self._e
+
+        first = True
+
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+
+            while right > 1 and right % 2:
+                right >>= 1
+
+            if not f(self._op(self._d[right], sm)):
+                while right < self._size:
+                    right = 2 * right + 1
+                    if f(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
 
 def main():
     import sys
@@ -9,27 +151,13 @@ def main():
 
     n = int(input())
     a = list(map(int, input().split()))
-    front, back = [a[0]], [a[-1]]
-
     # 一つの要素を書き換え = 左右から累積的に演算して、後から合成
-    for ai in a[1:]:
-        front.append(gcd(ai, front[-1]))
-
-    for ai in a[::-1][1:]:
-        back.append(gcd(ai, back[-1]))
-
-    back = back[::-1]
-
+    # セグメント木を使う
+    st = SegTree(gcd, 0, a)  # op, e, array
     ans = 1
 
     for i in range(n):
-        if i == 0:
-            candidate = back[i + 1]
-        elif i == n - 1:
-            candidate = front[i - 1]
-        else:
-            candidate = gcd(front[i - 1], back[i + 1])
-
+        candidate = gcd(st.prod(0, i), st.prod(i + 1, n))
         ans = max(ans, candidate)
 
     print(ans)
