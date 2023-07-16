7bf31a77b7023ffb0a4c04774493f68f3c9b6d66
Kato Hiroki <k.hiro1818@gmail.com>
2023-07-16 16:05:44 +0900

    Refactoring

--- ABC/abc301-abc350/abc310/c/main.py
+++ ABC/abc301-abc350/abc310/c/main.py
@@ -3,35 +3,22 @@
 
 def main():
     import sys
-    from collections import Counter
 
     input = sys.stdin.readline
 
     n = int(input())
     s = [input().rstrip() for _ in range(n)]
-    c = Counter(s)
-    d = Counter([si[::-1] for si in s])
     used = set()
-    ans = set()
+    ans = 0
 
-    for key in c.keys():
-        if len(key) == 1:
-            ans.add(key)
-            used.add(key)
+    for si in s:
+        if si not in used:
+            ans += 1
 
-        if key in used:
-            continue
+        used.add(si)
+        used.add(si[::-1])
 
-        rev_key = key[::-1]
-
-        if rev_key in d.keys():
-            used.add(rev_key)
-
-        ans.add(key)
-        used.add(key)
-
-    # print(d)
-    print(len(ans))
+    print(ans)
 
 
 if __name__ == "__main__":
