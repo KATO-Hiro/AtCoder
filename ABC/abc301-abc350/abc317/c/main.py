8b061567ede8ab9b49ef0ee0c98331bc691c1ee0
Kato Hiroki <k.hiro1818@gmail.com>
2023-08-28 07:28:03 +0900

    Refactoring

--- ABC/abc301-abc350/abc317/c/main.py
+++ ABC/abc301-abc350/abc317/c/main.py
@@ -3,7 +3,6 @@
 
 def main():
     import sys
-    from itertools import permutations
 
     input = sys.stdin.readline
 
@@ -20,20 +19,27 @@ def main():
         dist[bi][ai] = ci
 
     # print(dist)
-    ans = 0
 
-    # 頂点の順列を全列挙
-    for pattern in permutations(range(n)):
-        candidate = 0
+    # dp[s][v]: 訪問済みの頂点集合s、現在の頂点vとしたときの距離の最大値
+    inf = -(10**18)
+    dp = [[inf for _ in range(n)] for _ in range(1 << n)]
 
-        for frm, to in zip(pattern, pattern[1:]):
-            if dist[frm][to] == pending:
-                break
+    # 初期化: 頂点iを始点としたときの距離を0に
+    for i in range(n):
+        dp[1 << i][i] = 0
 
-            candidate += dist[frm][to]
+    for s in range(1 << n):
+        for v in range(n):
+            if dp[s][v] == inf:
+                continue
 
-        ans = max(ans, candidate)
+            for u in range(n):
+                # 未訪問 + 到達不可能な場合を除外
+                if not ((s >> u) & 1) and dist[v][u] != pending:
+                    dp[s | 1 << u][u] = max(dp[s | 1 << u][u], dp[s][v] + dist[v][u])
 
+    # 2次元のリストに対して、max(dp)とすると正しい答えが得られない
+    ans = max([max(dp_i) for dp_i in dp])
     print(ans)
 
 
