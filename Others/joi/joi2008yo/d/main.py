025068f45f1965e63209a0f3cd1571abbbe8eaa8
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-07 07:25:05 +0900

    Refactoring

--- Others/joi/joi2008yo/d/main.py
+++ Others/joi/joi2008yo/d/main.py
@@ -12,20 +12,23 @@ def main():
     xy2 = [tuple(map(int, input().split())) for _ in range(m)]
     xy2 = set(xy2)
 
-    for x1, y1 in xy1:
-        for x2, y2 in xy2:
-            dx, dy = x2 - x1, y2 - y1
-            count = 0
+    # 最も外側のループは事実上1ペアしか使っていないので、xy1の1点だけを利用すれば良い
+    # for x1, y1 in xy1:
+    for x2, y2 in xy2:
+        x1, y1 = xy1[0]
+        dx, dy = x2 - x1, y2 - y1
+        ok = True
 
-            for x3, y3 in xy1:
-                nx, ny = x3 + dx, y3 + dy
+        for x3, y3 in xy1:
+            nx, ny = x3 + dx, y3 + dy
 
-                if (nx, ny) in xy2:
-                    count += 1
+            if (nx, ny) not in xy2:
+                ok = False
+                break
 
-            if count == n:
-                print(dx, dy)
-                exit()
+        if ok:
+            print(dx, dy)
+            exit()
 
 
 if __name__ == "__main__":
