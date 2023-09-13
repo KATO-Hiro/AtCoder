bdd3eff6af1444239a584d184915c7f4fc09c5ae
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-14 07:49:11 +0900

    Upsolve ABC

---
+++ AtCoder_Virtual_Contest/mayocorn20230913/c/main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
+def compress_coordinate(elements: list) -> dict:
+    """Means that reduce the numerical value while maintaining the magnitude
+        relationship.
+
+    Args:
+        elements: list of integer numbers (greater than -1).
+
+    Returns:
+        A dictionary's items ((original number, compressed number) pairs).
+
+    Landau notation: O(n log n)
+    """
+
+    # See:
+    # https://atcoder.jp/contests/abc036/submissions/5707999?lang=ja
+    compressed_list = sorted(set(elements))
+    return {element: index for index, element in enumerate(compressed_list)}
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    h, w, n = map(int, input().split())
+    a, b = list(), list()
+
+    for _ in range(n):
+        ai, bi = map(int, input().split())
+        a.append(ai)
+        b.append(bi)
+
+    # print(a, b)
+
+    ca = compress_coordinate(a)
+    cb = compress_coordinate(b)
+    # print(ca)
+    # print(cb)
+
+    for ai, bi in zip(a, b):
+        print(ca[ai] + 1, cb[bi] + 1)
+
+
+if __name__ == "__main__":
+    main()
