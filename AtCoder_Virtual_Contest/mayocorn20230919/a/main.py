9fd9e20604e5877c80aa74958c9c1b117f6c41c4
Kato Hiroki <k.hiro1818@gmail.com>
2023-09-21 07:29:21 +0900

    Upsolve ABC

---
+++ AtCoder_Virtual_Contest/mayocorn20230919/a/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    h, w = map(int, input().split())
+    ans = 0
+
+    for _ in range(h):
+        si = input().rstrip()
+        ans += si.count("#")
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
