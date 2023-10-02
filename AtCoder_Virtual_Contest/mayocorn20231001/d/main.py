a34e5150ceba4192421fe2818a2fc640dc3aedc9
Kato Hiroki <k.hiro1818@gmail.com>
2023-10-03 06:23:30 +0900

    Solve joi 2024 yo1a C

---
+++ AtCoder_Virtual_Contest/mayocorn20231001/d/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    from collections import deque
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    s = input().rstrip()[::-1]
+
+
+if __name__ == "__main__":
+    main()
