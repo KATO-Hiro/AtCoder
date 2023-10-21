0bf5cd67a8b283a14d9986c6961db711591cd79d
Kato Hiroki <k.hiro1818@gmail.com>
2023-10-21 23:20:34 +0900

    Solve ABC325A

---
+++ ABC/abc301-abc350/abc325/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    s, t = input().rstrip().split()
+
+    print(s + " " + "san")
+
+
+if __name__ == "__main__":
+    main()
