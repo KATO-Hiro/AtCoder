993500e0bf7be0e107a1e1149658aa1f297d7e3d
Kato Hiroki <k.hiro1818@gmail.com>
2023-11-21 07:06:46 +0900

    Solve ABC329B

---
+++ ABC/abc301-abc350/abc329/b/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = sorted(set(map(int, input().split())), reverse=True)[1]
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
