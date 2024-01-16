260dddebda2175265b161893c750aaa6c0626955
Kato Hiroki <k.hiro1818@gmail.com>
2024-01-16 22:04:54 +0900

    Solve ABC336A

---
+++ ABC/abc301-abc350/abc336/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+
+    print("L" + "o" * n + "ng")
+
+
+if __name__ == "__main__":
+    main()
