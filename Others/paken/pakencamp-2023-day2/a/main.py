25e5618f89c6c716346aa1c0666cb6646f2f6290
Kato Hiroki <k.hiro1818@gmail.com>
2024-04-03 21:21:41 +0900

    Solve pakencamp2023 day2 A

---
+++ Others/paken/pakencamp-2023-day2/a/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+
+    if n == 1:
+        print("maspy")
+    elif n == 2:
+        print("square1001")
+    elif n == 3:
+        print("PCTprobability")
+
+
+if __name__ == "__main__":
+    main()
