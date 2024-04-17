fcc254cc514927429a14c85ba3f701ee58065e4d
Kato Hiroki <k.hiro1818@gmail.com>
2024-04-18 07:10:18 +0900

    Solve

---
+++ APG4bPython/co/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+
+    for _ in range(n):
+        ai, bi = map(int, input().split())
+        print(ai + bi)
+
+
+if __name__ == "__main__":
+    main()
