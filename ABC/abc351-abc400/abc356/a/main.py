ccea7b1827b36af8d02d307dfb4e26741a43613d
Kato Hiroki <k.hiro1818@gmail.com>
2024-06-04 22:03:15 +0900

    Refactoring

--- ABC/abc351-abc400/abc356/a/main.py
+++ ABC/abc351-abc400/abc356/a/main.py
@@ -7,11 +7,8 @@ def main():
     input = sys.stdin.readline
 
     n, l, r = map(int, input().split())
-    ans = (
-        [i for i in range(1, l)]
-        + [i for i in range(l, r + 1)][::-1]
-        + [i for i in range(r + 1, n + 1)]
-    )
+    ans = [i for i in range(1, n + 1)]
+    ans[l - 1 : r] = ans[l - 1 : r][::-1]
     print(*ans)
 
 
