105f23ba4a2cf626a974b91a069e5bbff49a21cb
Kato Hiroki <k.hiro1818@gmail.com>
2024-06-05 13:15:56 +0900

    UpSolve ABC356D

--- ABC/abc351-abc400/abc356/d/main.py
+++ ABC/abc351-abc400/abc356/d/main.py
@@ -9,7 +9,6 @@ def main():
     n, m = map(int, input().split())
     ans = 0
     mod = 998244353
-    n += 1
 
     # 各桁でkを2進数表記したときのビットが立っている個数を数える
     for i in range(60):
@@ -19,14 +18,14 @@ def main():
 
         # kを0から順番に並べると、0が2 ^ i個、1が2 ^ i個 (周期2 ^ (i + 1))並んでいる
         # 周期と端数を求める
-        period = 2 << i
+        period = 1 << (i + 1)
         remain = n % period
 
-        ans += (n // period) * period // 2
+        ans += (n - remain) // 2
         ans %= mod
 
         if remain >= (1 << i):
-            ans += remain - (1 << i)
+            ans += remain - (1 << i) + 1
             ans %= mod
 
     print(ans)
