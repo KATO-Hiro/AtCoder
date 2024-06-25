b85c1be38d118a10ce4ea9959bbdeedaf93a0a4f
Kato Hiroki <k.hiro1818@gmail.com>
2024-06-25 22:54:19 +0900

    Refactoring

--- ARC/arc151-arc200/arc152/a/main.py
+++ ARC/arc151-arc200/arc152/a/main.py
@@ -10,31 +10,15 @@ def main():
     a = list(map(int, input().split()))
 
     # 1つ席を空けながら前の組から座る
-    # 最後の2人組に到達したときに残りの席が2以上ならYes
-    k = n
-
-    for j in range(n - 1, -1, -1):
-        if a[j] == 2:
-            k = j
-            break
-
-    if k == n:
-        if a[-1] == 2:
+    # 2人組が座ろうとしたときに、残りの席が1以下ならNo
+    for ai in a:
+        if ai == 2 and l <= 1:
             print("No")
-        elif a[-1] == 1:
-            print("Yes")
+            exit()
 
-        exit()
+        l -= ai + 1
 
-    summed = 0
-
-    for i in range(k):
-        summed += a[i] + 1
-
-    if l - summed >= 2:
-        print("Yes")
-    else:
-        print("No")
+    print("Yes")
 
 
 if __name__ == "__main__":
