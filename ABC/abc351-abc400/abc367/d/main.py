c90353da1ba825ef6492477e6765bb300b442856
Kato Hiroki <k.hiro1818@gmail.com>
2024-08-18 08:13:02 +0900

    Refactoring

--- ABC/abc351-abc400/abc367/d/main.py
+++ ABC/abc351-abc400/abc367/d/main.py
@@ -10,24 +10,25 @@ def main():
 
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
-    b = list(accumulate(a + a, initial=0))
+    acc = list(accumulate(a + a, initial=0))
     d = defaultdict(int)
 
     # 数列がmの倍数 => mod mの頻度を数える
     # mod mが同じ個数を数える + 差分更新
-    for i in range(n):
-        q = b[i] % m
+    for i in range(1, n):
+        q = acc[i] % m
         d[q] += 1
 
-    ans = d[b[0]] - 1
+    ans = 0
 
-    for j in range(1, n):
-        q = b[j - 1] % m
+    # 答え → 頻度の順番で更新すると、シンプルに
+    for j in range(n):
+        ans += d[acc[j] % m]
+
+        q = acc[j + 1] % m
         d[q] -= 1
-
-        nq = b[j + n - 1] % m
+        nq = acc[j + n] % m
         d[nq] += 1
-        ans += d[b[j] % m] - 1
 
     print(ans)
 
