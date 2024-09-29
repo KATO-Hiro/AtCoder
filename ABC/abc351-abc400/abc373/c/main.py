6060d627de5c340805a70b219201465634ef5552
Kato Hiroki <k.hiro1818@gmail.com>
2024-09-30 06:16:11 +0900

    Solve ABC373C

---
+++ ABC/abc351-abc400/abc373/c/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    print(max(a) + max(b))
+
+
+if __name__ == "__main__":
+    main()
