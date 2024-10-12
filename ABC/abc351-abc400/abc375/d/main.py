1945970b6b91fde91d3a5c2e1980ddd75ed6d093
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-13 07:13:46 +0900

    Refactoring

--- ABC/abc351-abc400/abc375/d/main.py
+++ ABC/abc351-abc400/abc375/d/main.py
@@ -8,15 +8,13 @@ def main():
     input = sys.stdin.readline
 
     s = input().rstrip()
-    t = s[::-1]
     n = len(s)
     alpha = ascii_uppercase
     alpha_count = len(ascii_uppercase)
 
     # 3要素のうち真ん中を固定
-    # 文字は26種類なので、前計算で左右から累積和を取る
+    # 文字は26種類なので、前計算で左から累積和を取る
     acc_from_left = [[0] * n for _ in range(alpha_count)]
-    acc_from_right = [[0] * n for _ in range(alpha_count)]
 
     for i, a in enumerate(alpha):
         for j, si in enumerate(s):
@@ -26,21 +24,14 @@ def main():
             if si == a:
                 acc_from_left[i][j] += 1
 
-    for i, a in enumerate(alpha):
-        for j, ti in enumerate(t):
-            if j >= 1:
-                acc_from_right[i][j] = acc_from_right[i][j - 1]
-
-            if ti == a:
-                acc_from_right[i][j] += 1
-
-        acc_from_right[i] = acc_from_right[i][::-1]
-
     ans = 0
 
     for j in range(1, n - 1):
         for i in range(alpha_count):
-            ans += acc_from_left[i][j - 1] * acc_from_right[i][j + 1]
+            left = acc_from_left[i][j - 1]
+            right = acc_from_left[i][n - 1] - acc_from_left[i][j]
+
+            ans += left * right
 
     print(ans)
 
