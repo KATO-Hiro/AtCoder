538e85a8dc7064d9869eab3c2ed58df8621fcbd3
Kato Hiroki <k.hiro1818@gmail.com>
2024-10-15 06:21:44 +0900

    Refactoring

--- ABC/abc351-abc400/abc375/b/main.py
+++ ABC/abc351-abc400/abc375/b/main.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
-def dist(x1, y1, x2, y2):
-    from math import sqrt
-
-    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-
 def main():
     import sys
     from itertools import pairwise
+    from math import hypot
 
     input = sys.stdin.readline
 
@@ -18,7 +13,7 @@ def main():
     ans = 0
 
     for (x1, y1), (x2, y2) in pairwise(xy):
-        ans += dist(x1, y1, x2, y2)
+        ans += hypot(x2 - x1, y2 - y1)
 
     print(ans)
 
