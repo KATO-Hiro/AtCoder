cb82696a299fd793d20d4558771b88ef367f485e
Kato Hiroki <k.hiro1818@gmail.com>
2024-11-28 07:29:11 +0900

    Solve joi2025 yo1c A

---
+++ Others/joi/joi2025yo1c/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    a = int(input())
+    b = int(input())
+    print(1000 * a + 10000 * b)
+
+
+if __name__ == "__main__":
+    main()
