9bde6a5e6fb186bb0632f5537ee9b61e9a654b0c
Kato Hiroki <k.hiro1818@gmail.com>
2025-05-10 22:32:31 +0900

    Refactoring

--- ABC/abc401-abc450/abc404/a/main.py
+++ ABC/abc401-abc450/abc404/a/main.py
@@ -1,59 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-def get_offset(alphabet: str, base_alphabet: str = "A") -> int:
-    """Get offset between the base_alphabet and alphabet.
-
-    Args:
-        alphabet: The alphabet to use.
-        base_alphabet: The base alphabet to use.
-
-    Returns:
-        difference between the base_alphabet and alphabet.
-
-    See:
-    https://docs.python.org/3.11/library/functions.html?highlight=chr#ord
-    """
-
-    return ord(alphabet) - ord(base_alphabet)
-
-
-def add_offset_to_alphabet(offset: int, base_alphabet: str = "A") -> str:
-    """Add offset to the base_alphabet.
-
-    Args:
-        offset: Difference from the base alphabet.
-        base_alphabet: The base alphabet to use.
-
-    Returns:
-        Corrected alphabet.
-
-    See:
-    https://docs.python.org/3.11/library/functions.html?highlight=chr#ord
-    """
-
-    return chr(ord(base_alphabet) + offset)
-
-
 def main():
     import sys
+    from string import ascii_lowercase
 
     input = sys.stdin.readline
 
     s = input().rstrip()
-    used = [False] * 26
 
-    for si in s:
-        offset = get_offset(si, "a")
-        used[offset] = True
-
-    for i, u in enumerate(used):
-        if u:
-            continue
-
-        ans = add_offset_to_alphabet(i, "a")
-        print(ans)
-        break
+    for alpha in ascii_lowercase:
+        if alpha not in s:
+            print(alpha)
+            exit()
 
 
 if __name__ == "__main__":
