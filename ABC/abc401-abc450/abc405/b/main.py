22adcbd3099aae526d6d8de97b36a99e42993705
Kato Hiroki <k.hiro1818@gmail.com>
2025-05-13 22:12:07 +0900

    Refactoring

--- ABC/abc401-abc450/abc405/b/main.py
+++ ABC/abc401-abc450/abc405/b/main.py
@@ -3,29 +3,18 @@
 
 def main():
     import sys
-    from collections import defaultdict
 
     input = sys.stdin.readline
 
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
-    d = defaultdict(int)
 
-    for ai in a:
-        d[ai] += 1
-
-    for i in range(1, m + 1):
-        if d[i] == 0:
-            print(0)
+    for i in range(n + 1):
+        if len(set(a)) < m:
+            print(i)
             exit()
 
-    for i, ai in enumerate(a[::-1], 1):
-        d[ai] -= 1
-
-        for j in range(1, m + 1):
-            if d[j] == 0:
-                print(i)
-                exit()
+        a.pop()
 
 
 if __name__ == "__main__":
