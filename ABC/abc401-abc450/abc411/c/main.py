1155e2afb8a3f85c397bf27f1261d97fc81635b2
Kato Hiroki <k.hiro1818@gmail.com>
2025-06-23 22:12:59 +0900

    UpSolve

--- ABC/abc401-abc450/abc411/c/main.py
+++ ABC/abc401-abc450/abc411/c/main.py
@@ -8,25 +8,14 @@ def main():
 
     n, q = map(int, input().split())
     a = list(map(int, input().split()))
-    b = [-1] + ([0] * n) + [-1]
+    b = [0] * (n + 2)
     count = 0
 
     for ai in a:
-        if 2 <= ai < n:
-            if b[ai - 1] == b[ai] == b[ai + 1]:
-                count += 1
-            elif b[ai - 1] == b[ai + 1]:
-                count -= 1
-        elif ai == 1:
-            if b[ai] == 0 and b[ai + 1] != 1:
-                count += 1
-            elif b[ai] == 1 and b[ai + 1] != 1:
-                count -= 1
-        elif ai == n:
-            if b[ai - 1] != 1 and b[ai] == 0:
-                count += 1
-            elif b[ai - 1] != 1 and b[ai] == 1:
-                count -= 1
+        if b[ai - 1] == b[ai] == b[ai + 1]:
+            count += 1
+        elif b[ai - 1] == b[ai + 1]:
+            count -= 1
 
         b[ai] ^= 1
 
