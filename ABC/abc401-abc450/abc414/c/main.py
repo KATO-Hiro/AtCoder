9518bffd97fbb7fd99e490b2af3e37c848145034
Kato Hiroki <k.hiro1818@gmail.com>
2025-07-15 22:16:22 +0900

    UpSolve ABC414C

--- ABC/abc401-abc450/abc414/c/main.py
+++ ABC/abc401-abc450/abc414/c/main.py
@@ -2,17 +2,6 @@
 
 
 def convert_decimal_to_n_ary_number(n: int, m_ary_number: int = 2) -> str:
-    """Represents conversion from decimal number to n-ary number.
-
-    Args:
-        n: Input number (greater than 1).
-        m_ary_number: m-ary number (from 2 to 10).
-
-    Returns:
-        values of m-ary number.
-
-    Landau notation: O(log n)
-    """
     if n < m_ary_number:
         return str(n)
 
@@ -38,38 +27,28 @@ def main():
     n = int(input())
     ans = 0
 
-    # 偶数桁
     for i in range(1, 10**6):
         si = str(i)
+
+        # 偶数桁
         s = si + si[::-1]
         s2 = int(s)
 
-        if s2 > n:
-            break
+        if s2 <= n:
+            result = convert_decimal_to_n_ary_number(s2, a)
 
-        result = convert_decimal_to_n_ary_number(s2, a)
+            if result == result[::-1]:
+                ans += s2
 
-        if result == result[::-1]:
-            ans += s2
+        # 奇数桁
+        # 中央の一つを削除
+        t = si[:-1] + si[::-1]
+        t2 = int(t)
 
-    # 奇数桁
-    for j in range(10**6):
-        for center in range(10):
-            tj = str(j)
+        if t2 <= n:
+            result = convert_decimal_to_n_ary_number(t2, a)
 
-            if j == 0:
-                t = str(center)
-            else:
-                t = tj + str(center) + tj[::-1]
-
-            t2 = int(t)
-
-            if t2 > n:
-                break
-
-            result2 = convert_decimal_to_n_ary_number(t2, a)
-
-            if result2 == result2[::-1]:
+            if result == result[::-1]:
                 ans += t2
 
     print(ans)
