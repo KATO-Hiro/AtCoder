ef700c393a2dae9165df79c811b61f2004d842bd
Kato Hiroki <k.hiro1818@gmail.com>
2025-08-11 22:15:20 +0900

    UpSolve ABC418D (#4959)

--- ABC/abc401-abc450/abc418/d/main.py
+++ ABC/abc401-abc450/abc418/d/main.py
@@ -12,12 +12,18 @@ def main():
     dp = [0] * 2
     ans = 0
 
-    for ti in t:
-        if ti == "0":
-            dp[0], dp[1] = dp[1], dp[0]
+    for i in range(1, n + 1):
+        ndp = [0] * 2
 
-        dp[int(ti)] += 1
-        ans += dp[1]
+        if t[i - 1] == "0":
+            ndp[0] = dp[1]
+            ndp[1] = dp[0] + 1
+        else:
+            ndp[0] = dp[0] + 1
+            ndp[1] = dp[1]
+
+        ans += ndp[0]
+        dp = ndp
 
     print(ans)
 
