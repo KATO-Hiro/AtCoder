34d01ab8e600e9f45f6e7bdc01e684c4d5fd0c70
Kato Hiroki <k.hiro1818@gmail.com>
2025-08-20 15:45:31 +0900

    UpSolve ABC419C (#4982)

--- ABC/abc401-abc450/abc419/c/main.py
+++ ABC/abc401-abc450/abc419/c/main.py
@@ -17,25 +17,10 @@ def main():
         c_min = min(c_min, ci)
         c_max = max(c_max, ci)
 
-    r_center = (r_min + r_max) // 2
-    c_center = (c_min + c_max) // 2
-    ans = inf
+    r = r_max - r_min
+    c = c_max - c_min
 
-    for y in range(r_center - 2, r_center + 3):
-        for x in range(c_center - 2, c_center + 3):
-            candidates = 0
-
-            for yi, xi in rc:
-                dy = abs(y - yi)
-                dx = abs(x - xi)
-                d = min(dy, dx)
-
-                candidate = dy + dx - d
-                candidates = max(candidates, candidate)
-
-            ans = min(ans, candidates)
-
-    print(ans)
+    print((max(r, c) + 1) // 2)
 
 
 if __name__ == "__main__":
