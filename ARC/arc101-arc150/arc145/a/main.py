c6c407986eab659ab5fcd1fdcce54538386c4fdc
Kato Hiroki <k.hiro1818@gmail.com>
2025-10-15 08:42:02 +0900

    UpSolve ARC145A (#5088)

---
+++ ARC/arc101-arc150/arc145/a/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    s = input().rstrip()
+
+    if s != "BA" and (s[0] == "B" or s[-1] == "A"):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
