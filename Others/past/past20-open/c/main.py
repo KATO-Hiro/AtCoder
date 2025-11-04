a1823c0aef044d21e2bb96c6cf3611050dd69c08
Kato Hiroki <k.hiro1818@gmail.com>
2025-11-04 22:01:17 +0900

    Solve past 20th C (#5132)

--- Others/past/past20-open/c/main.py
+++ Others/past/past20-open/c/main.py
@@ -7,17 +7,8 @@ def main():
     input = sys.stdin.readline
 
     s = input().rstrip()
-    pos = list()
-
-    for i, si in enumerate(s):
-        if si != "X":
-            continue
-
-        pos.append(i)
-
-    front, back = pos
-    ans = s[:front] + s[front + 1 : back][::-1] + s[back + 1 :]
-    print(ans)
+    s1, s2, s3 = s.split("X")
+    print(s1 + s2[::-1] + s3)
 
 
 if __name__ == "__main__":
