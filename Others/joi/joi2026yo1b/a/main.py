aa4aa9ebb773a427554c9c6807ec029b78f48c0d
Kato Hiroki <k.hiro1818@gmail.com>
2025-11-21 21:54:14 +0900

    Solve joi2026yo1b A (#5164)

---
+++ Others/joi/joi2026yo1b/a/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+
+    input = sys.stdin.readline
+
+    a = int(input())
+    b = int(input())
+    print(a * b)
+
+
+if __name__ == "__main__":
+    main()
