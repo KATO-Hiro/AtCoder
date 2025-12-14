d77f65d4eaacb274ad7374a08606ddd6fd616df3
Kato Hiroki <k.hiro1818@gmail.com>
2025-12-14 09:56:22 +0900

    UpSolve

--- ARC/arc101-arc150/arc149/a/main.py
+++ ARC/arc101-arc150/arc149/a/main.py
@@ -5,31 +5,25 @@ def main():
     import sys
 
     input = sys.stdin.readline
-    sys.set_int_max_str_digits(0)
 
     n, m = map(int, input().split())
-    pending = -1
-    ans = [pending]
+    ans = (0, 0)
 
     for base in range(1, 10):
         number = base
-        candidate = pending
 
         for digit in range(1, n + 1):
             if number % m == 0:
-                candidate = str(base) * digit
+                ans = max(ans, (digit, base))
 
             number *= 10
             number += base
             number %= m
 
-        if candidate == pending:
-            continue
-
-        ans += [int(candidate)]
-
-    ans.sort()
-    print(ans[-1])
+    if ans == (0, 0):
+        print(-1)
+    else:
+        print(str(ans[1]) * ans[0])
 
 
 if __name__ == "__main__":
