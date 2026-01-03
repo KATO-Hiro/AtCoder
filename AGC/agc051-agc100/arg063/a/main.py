1349ce72da4a74d0f3711c00d5ea27d55ff9c55b
Kato Hiroki <k.hiro1818@gmail.com>
2026-01-03 14:23:32 +0900

    UpSolve AGC063A (#5227)

--- AGC/agc051-agc100/arg063/a/main.py
+++ AGC/agc051-agc100/arg063/a/main.py
@@ -10,31 +10,17 @@ def main():
     s = input().rstrip()
     a_count, b_count = 0, 0
 
-    if s[0] == "A":
-        a_count += 1
-    else:
-        b_count += 1
+    for i, si in enumerate(s):
+        a_count += si == "A"
+        b_count += si == "B"
 
-    for i, si in enumerate(s[1:], 1):
-        if si == "A":
-            a_count += 1
+        if i == 0:
+            continue
+
+        if a_count >= b_count:
+            print("Alice")
         else:
-            b_count += 1
-
-        if i % 2 == 1:
-            b_count -= 1
-
-            if b_count <= 0:
-                print("Alice")
-            else:
-                print("Bob")
-        else:
-            a_count -= 1
-
-            if a_count <= 0:
-                print("Bob")
-            else:
-                print("Alice")
+            print("Bob")
 
 
 if __name__ == "__main__":
