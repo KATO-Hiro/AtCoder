a03a99b894d03eb09783234733aca5e3d7734f7d
Kato Hiroki <k.hiro1818@gmail.com>
2026-06-01 21:42:13 +0900

    UpSolve AWC81

---
+++ AWC/awc0051-awc0100/awc0081/c/main.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    import sys
+    from collections import deque
+    from typing import List, Tuple
+
+    input = sys.stdin.readline
+
+    n, m, s = map(int, input().split())
+    graph = [[] for _ in range(n)]
+
+    for _ in range(m):
+        ai, bi = map(int, input().split())
+        ai -= 1
+        bi -= 1
+
+        graph[ai].append(bi)
+
+    inf = 10**18
+
+    def bfs_for_graph(
+        vertex_count: int, graph: List[List[int]], start_id: int
+    ) -> Tuple[List[bool], List[int]]:
+        d = deque([start_id])
+        visited = [False] * vertex_count
+        dist = [inf] * vertex_count
+        dist[start_id] = 0
+
+        while d:
+            cur = d.popleft()
+
+            if visited[cur]:
+                continue
+
+            visited[cur] = True
+
+            for to in graph[cur]:
+                if visited[to]:
+                    continue
+
+                d.append(to)
+                dist[to] = min(dist[to], dist[cur] + 1)
+
+        return visited, dist
+
+    start_id = s - 1
+    visited, dist = bfs_for_graph(vertex_count=n, graph=graph, start_id=start_id)
+    ans = max(dist)
+
+    if ans == inf:
+        ans = -1
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
