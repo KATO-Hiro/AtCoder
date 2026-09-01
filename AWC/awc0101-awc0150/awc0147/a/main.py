9a8b9956bfcd32cb077248a0a2145cef62fbb36a
Kato Hiroki <k.hiro1818@gmail.com>
2026-09-02 06:50:57 +0900

    UpSolve AWC147

--- AWC/awc0101-awc0150/awc0147/a/main.py
+++ AWC/awc0101-awc0150/awc0147/a/main.py
@@ -8,23 +8,12 @@ def main():
 
     n = int(input())
     a = list(map(int, input().split()))
-    b = [(ai, i) for i, ai in enumerate(a, 1)]
+    a_max = max(a)
 
-    while len(b) > 1:
-        m = len(b)
-        c = []
-
-        for i in range(m // 2):
-            b1, b2 = b[2 * i], b[2 * i + 1]
-
-            if b1[0] > b2[0]:
-                c.append(b1)
-            else:
-                c.append(b2)
-
-        b = c
-
-    print(b[0][1])
+    for i, ai in enumerate(a, 1):
+        if ai == a_max:
+            print(i)
+            exit()
 
 
 if __name__ == "__main__":
